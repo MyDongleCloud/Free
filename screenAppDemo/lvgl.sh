e8a584d29af0d66ff4965b583b16ccec1e1dfbaa
Gregoire Gentil <gregoire@gentil.com>
2025-04-19 20:19:59 -0700

    Modify LVGL build script to reflect latest folder name change

--- screenAppDemo/lvgl.sh
+++ screenAppDemo/lvgl.sh
@@ -5,7 +5,7 @@ rm -rf lvgl
 git clone https://github.com/lvgl/lvgl
 cd lvgl
 git checkout a78c4a476ebc58f5f754c25b25d8374c71aa0add
-ln -s ../../screenApp/lv_conf.h
+ln -s ../../screenAppDemo/lv_conf.h
 mkdir build
 cd build
 cmake ..
