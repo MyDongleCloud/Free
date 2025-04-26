7d187e285e26033ebc5f44963c0dde53325b4916
Gregoire Gentil <gregoire@gentil.com>
2025-04-25 21:47:02 -0700

    Add check that mydonglecloud-init.sh has not yet been done

--- rootfs/usr/bin/mydonglecloud-init.sh
+++ rootfs/usr/bin/mydonglecloud-init.sh
@@ -73,24 +73,26 @@ else
 fi
 
 if [ $INIT = 1 ]; then
-	ln -sf /sys/devices/platform/mydonglecloud /dev/mydonglecloud_platform
-	chmod 222 /dev/mydonglecloud_platform/printk
-	chmod 222 /dev/mydonglecloud_platform/buzzer
-	chmod 222 /dev/mydonglecloud_platform/buzzerClick
-	chmod 222 /dev/mydonglecloud_platform/buzzerFreq
-	chmod 444 /dev/mydonglecloud_platform/hardwareVersion
-	chmod 444 /dev/mydonglecloud_platform/serialNumber
+	if [ ! -d /dev/mydonglecloud_platform ]; then
+		ln -sf /sys/devices/platform/mydonglecloud /dev/mydonglecloud_platform
+		chmod 222 /dev/mydonglecloud_platform/printk
+		chmod 222 /dev/mydonglecloud_platform/buzzer
+		chmod 222 /dev/mydonglecloud_platform/buzzerClick
+		chmod 222 /dev/mydonglecloud_platform/buzzerFreq
+		chmod 444 /dev/mydonglecloud_platform/hardwareVersion
+		chmod 444 /dev/mydonglecloud_platform/serialNumber
 
-	ln -sf /sys/bus/spi/devices/spi0.0 /dev/mydonglecloud_screen
-	chmod 666 /dev/mydonglecloud_screen/backlight
-	chmod 666 /dev/mydonglecloud_screen/rotation
-	chmod 222 /dev/mydonglecloud_screen/reset
-	chmod 222 /dev/mydonglecloud_screen/init
-	chmod 222 /dev/mydonglecloud_screen/rect
-	chmod 222 /dev/mydonglecloud_screen/update
-	chmod 666 /dev/mydonglecloud_screen_f
+		ln -sf /sys/bus/spi/devices/spi0.0 /dev/mydonglecloud_screen
+		chmod 666 /dev/mydonglecloud_screen/backlight
+		chmod 666 /dev/mydonglecloud_screen/rotation
+		chmod 222 /dev/mydonglecloud_screen/reset
+		chmod 222 /dev/mydonglecloud_screen/init
+		chmod 222 /dev/mydonglecloud_screen/rect
+		chmod 222 /dev/mydonglecloud_screen/update
+		chmod 666 /dev/mydonglecloud_screen_f
 
-	/usr/bin/mydonglecloud-otg.sh
+		/usr/bin/mydonglecloud-otg.sh
 
-	echo "Init done"
+		echo "Init done"
+	fi
 fi
