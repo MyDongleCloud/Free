b2fe814d70c7129b1e46b3cd0091bc71c5c5f394
Gregoire Gentil <gregoire@gentil.com>
2025-05-10 21:14:15 -0700

    Improve image install script

--- private/install.sh
+++ private/install.sh
@@ -9,8 +9,6 @@ exit 0
 }
 
 PROD=0
-OS="pios"
-OS="ubuntu"
 while getopts hp opt; do
 	case "$opt" in
 		h) helper;;
@@ -23,6 +21,18 @@ if [ "m`id -u`" != "m0" ]; then
 	exit 0
 fi
 
+lsb_release -a | grep bookworm
+if [ $? = 0 ]; then
+	OS="pios"
+fi
+lsb_release -a | grep noble
+if [ $? = 0 ]; then
+	OS="ubuntu"
+fi
+
+echo "################################"
+echo "Starter"
+echo "################################"
 mkdir /home/mdc/build
 sed -i -e 's|/root|/home/mdc|' /etc/passwd
 rm -rf /root
@@ -37,19 +47,33 @@ ln -sf /etc/systemd/system/mydonglecloud-init.service /etc/systemd/system/sysini
 echo -n " modules-load=dwc2,libcomposite,configs,mydonglecloud" >> /boot/firmware/cmdline.txt
 echo -e "dtoverlay=dwc2\ndtoverlay=mydonglecloud\ndtoverlay=st7735s\ndtoverlay=buttons\ndtoverlay=leds\ndtparam=uart0=on\ndtparam=spi=on" >> /boot/firmware/config.txt
 adduser --comment Administrator --disabled-password admin
+
+echo "################################"
+echo "Upgrade"
+echo "################################"
 apt-get update
 apt-get -y upgrade
+
+echo "################################"
+echo "Basic"
+echo "################################"
 apt-get -y install liboath-dev libinput-dev libboost-dev libboost-system-dev libboost-thread-dev libboost-filesystem-dev libcurl4-openssl-dev libssl-dev libbluetooth-dev
 apt-get -y install composer apache2 php php-mysql libapache2-mod-php sqlite3 postfix procmail rspamd dovecot-pop3d dovecot-imapd
 apt-get -y install evtest qrencode dos2unix lrzsz imagemagick squashfs-tools libpam-oath oathtool
 if [ $OS = "ubuntu" ]; then
+	chmod a-x /etc/update-motd.d/*
 	snap remove snapd
 	apt-get -y purge snapd
 	apt-get -y install bzip2 zip gpiod net-tools wireless-tools build-essential cmake
-	apt-get -y install linux-headers-raspi linux-image-raspi
-	apt-get -y install mysql-server
+fi
+
+echo "################################"
+echo "pcpp"
+echo "################################"
+if [ $OS = "ubuntu" ]; then
 	apt-get -y install python3-pcpp
 	ln -sf pcpp-python /usr/bin/pcpp
+fi
 elif [ $OS = "pios" ]; then
 	cd /home/mdc/build
 	wget https://files.pythonhosted.org/packages/41/07/876153f611f2c610bdb8f706a5ab560d888c938ea9ea65ed18c374a9014a/pcpp-1.30.tar.gz
@@ -57,15 +81,89 @@ elif [ $OS = "pios" ]; then
 	cd pcpp-1.30
 	python setup.py install
 fi
+
+echo "################################"
+echo "pyupdi"
+echo "################################"
 cd /home/mdc/build
 git clone https://github.com/mraardvark/pyupdi
 cd pyupdi
 python setup.py install
 cd ../..
-#apache enable modules
-chmod a-x /etc/update-motd.d/*
+
+echo "################################"
+echo "Mysql"
+echo "################################"
+if [ $OS = "ubuntu" ]; then
+	apt-get -y install mysql-server
+elif [ $OS = "pios" ]; then
+	apt-get -y install libaio1 libevent-pthreads-2.1-7 libmecab2
+	wget http://ports.ubuntu.com/pool/main/i/icu/libicu70_70.1-2ubuntu1_arm64.deb
+	wget http://ports.ubuntu.com/pool/main/p/protobuf/libprotobuf-lite23_3.12.4-1ubuntu7.22.04.2_arm64.deb
+	wget http://ports.ubuntu.com/pool/main/m/mysql-defaults/mysql-common_5.8+1.0.8_all.deb
+	wget http://ports.ubuntu.com/pool/main/m/mysql-8.0/mysql-server-core-8.0_8.0.42-0ubuntu0.22.04.1_arm64.deb
+	wget http://ports.ubuntu.com/pool/main/m/mysql-8.0/mysql-server-8.0_8.0.42-0ubuntu0.22.04.1_arm64.deb
+	wget http://ports.ubuntu.com/pool/main/m/mysql-8.0/mysql-client-core-8.0_8.0.42-0ubuntu0.22.04.1_arm64.deb
+	wget http://ports.ubuntu.com/pool/main/m/mysql-8.0/mysql-client-8.0_8.0.42-0ubuntu0.22.04.1_arm64.deb
+	dpkg -i libicu70* libprotobuf-lite23* mysql-common*
+	dpkg -i mysql-client*
+	dpkg -i mysql-server*
+fi
+
+echo "################################"
+echo "Docker"
+echo "################################"
+if [ $OS = "ubuntu" ]; then
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+	echo "deb [arch=arm64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu noble stable" > /etc/apt/sources.list.d/docker.list
+elif [ $OS = "pios" ]; then
+	curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+	echo "deb [arch=arm64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian bookworm stable" > /etc/apt/sources.list.d/docker.list
+fi
+apt-get update
+apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+usermod -aG docker admin
+
+echo "################################"
+echo "Qdrant"
+echo "################################"
+if [ 0 = 1 ]; then
+	cd /tmp
+	wget https://github.com/qdrant/qdrant/releases/download/v1.13.6/qdrant-aarch64-unknown-linux-musl.tar.gz
+	tar -xpvf qdrant-aarch64-unknown-linux-musl.tar.gz
+	rm -f qdrant-aarch64-unknown-linux-musl.tar.gz
+	mv qdrant /usr/bin
+	chmod a+x /usr/bin/qdrant
+fi
+
+echo "################################"
+echo "uMTP"
+echo "################################"
+cd /home/mdc/build
+git clone https://github.com/viveris/uMTP-Responder
+cd uMTP-Responder
+make
+cp umtprd /usr/bin
+cd ../..
+
+echo "################################"
+echo "Kernel"
+echo "################################"
+if [ $OS = "ubuntu" ]; then
+	apt-get -y install linux-headers-raspi linux-image-raspi
+elif [ $OS = "pios" ]; then
+	apt-get -y install
+fi
+
+echo "################################"
+echo "Cleanup"
+echo "################################"
 apt-get -y autoremove
 rm /var/cache/apt/archives/*.deb
+
+echo "################################"
+echo "App and rootfs"
+echo "################################"
 cd /home/mdc
 tar -xjpvf /tmp/a.tbz2
 cd kernel && make && make install && cd ..
@@ -73,19 +171,6 @@ cd app && ./lvgl.sh && make && cd ..
 chown -R root:root rootfs
 cp -a rootfs/* /
 rm -rf rootfs
-
-#cd build
-#git clone https://github.com/viveris/uMTP-Responder
-#cd uMTP-Responder
-#make
-#cp umtprd /usr/bin
-
-#cd /tmp
-#wget https://github.com/qdrant/qdrant/releases/download/v1.13.6/qdrant-aarch64-unknown-linux-musl.tar.gz
-#tar -xpvf qdrant-aarch64-unknown-linux-musl.tar.gz
-#rm -f qdrant-aarch64-unknown-linux-musl.tar.gz
-#mv qdrant /usr/bin
-#chmod a+x /usr/bin/qdrant
 
 if [ $PROD = 1 ]; then
 	sed -i -e 's|mdc:[^:]*:|mdc:*:|' /etc/shadow
