ec820228f117893faec11503fa71b80ad14e638c
Gregoire Gentil <gregoire@gentil.com>
2025-05-12 18:38:59 -0700

    Add basic website

---
+++ www/js/custom.js
@@ -0,0 +1,56 @@
+(function($) {
+    "use strict";
+	
+	/* ..............................................
+	Loader 
+    ................................................. */
+	
+	$(window).on('load', function() { 
+		$('.preloader').fadeOut(); 
+		$('#preloader').delay(55).fadeOut('slow'); 
+		$('body').delay(45).css({'overflow':'visible'});
+	});
+    	
+	/* ..............................................
+    Navbar Bar
+    ................................................. */
+	
+	$('.navbar-nav .nav-link').on('click', function() {
+		var toggle = $('.navbar-toggler').is(':visible');
+		if (toggle) {
+			$('.navbar-collapse').collapse('hide');
+		}
+	});
+	
+	/* ..............................................
+    Fixed Menu
+    ................................................. */
+    
+	$(window).on('scroll', function () {
+		if ($(window).scrollTop() > 50) {
+			$('.top-header').addClass('fixed-menu');
+		} else {
+			$('.top-header').removeClass('fixed-menu');
+		}
+	});
+
+	/* ..............................................
+    Properties Filter
+    ................................................. */
+	var Container = $('.container');
+	Container.imagesLoaded(function () {
+		var portfolio = $('.properties-menu');
+		portfolio.on('click', 'button', function () {
+			$(this).addClass('active').siblings().removeClass('active');
+			var filterValue = $(this).attr('data-filter');
+			$grid.isotope({
+				filter: filterValue
+			});
+		});
+		var $grid = $('.properties-list').isotope({
+			itemSelector: '.properties-grid'
+		});
+
+	});
+	
+}(jQuery));
