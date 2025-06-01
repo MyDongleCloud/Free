1d23a296ffe93b83c10eacb4da7bec9ec7afc22e
Gregoire Gentil <gregoire@gentil.com>
2025-05-31 21:26:05 -0700

    Add a burn.sh script to burn a disk with the image

---
+++ private/burn.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+helper() {
+echo "*******************************************************"
+echo "Usage for burn.sh [-d disk -i img -h]"
+echo "d disk:	set /dev/disk"
+echo "d disk:	set image to burn"
+echo "h:		Print this usage and exit"
+exit 0
+}
+
+DISK=/dev/sdcard
+IMG=/work/ai.mydonglecloud/private/img/flasher-m-s.img
+while getopts d:i:h opt; do
+	case "$opt" in
+		d) DISK="/dev/${OPTARG}";;
+		i) IMG="${OPTARG}";;
+		h) helper;;
+	esac
+done
+
+umount ${DISK}*
+umount ${DISK}*
+dd if=${IMG} of=${DISK} bs=31M status=progress
+sync
+umount ${DISK}*
+umount ${DISK}*
+growpart ${DISK}0 2
+e2fsck -f -p ${DISK}2
+resize2fs ${DISK}2
+e2fsck -f -p ${DISK}2
+mount ${DISK}2 /tmp/2
+umount ${DISK}*
+umount ${DISK}*
