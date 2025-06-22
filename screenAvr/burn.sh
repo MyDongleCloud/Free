6f9a608a62cc504ae12697a14dae37a76ccf1f2e
Gregoire Gentil <gregoire@gentil.com>
2025-06-22 00:38:49 -0700

    Make work screen controlled by Attiny on Dongle Pro

---
+++ screenAvr/burn.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+helper() {
+echo "*******************************************************"
+echo "Usage of burn.sh [-e -f -h -s]"
+echo "e:	Erase"
+echo "f:	Flash"
+echo "h:	Print this usage and exit"
+echo "s:	Setup"
+exit 0
+}
+
+PP=/dev/ttyUSB0
+MCU=attiny202
+MEDIUM=uart
+ACTION=0
+SETUP=0
+while getopts efhs opt; do
+	case "$opt" in
+		e) ACTION=1;;
+		f) ACTION=2;;
+		h) helper;;
+		s) SETUP=1;;
+	esac
+done
+
+if [ $SETUP = 1 ]; then
+	~/Executables/pip.sh -s
+	PATH=/tmp/test/bin:$PATH
+	pip install pymcuprog
+	exit 0
+fi
+PATH=/tmp/test/bin:$PATH
+
+if [ $ACTION = 0 ]; then
+	pymcuprog ping -t $MEDIUM -u $PP -d $MCU
+fi
+if [ $ACTION = 1 ]; then
+	pymcuprog erase -t $MEDIUM -u $PP -d $MCU
+fi
+if [ $ACTION = 2 ]; then
+	pymcuprog write -t $MEDIUM -u $PP -d $MCU -f main.hex --erase
+fi
--- screenAvr/main.c
+++ screenAvr/main.c
@@ -1,38 +1,37 @@
 /*
      VDD 1|‾‾‾‾‾‾‾|8 GND
-     PA6 2|      |7 PA3 CLK
-     PA7 3|      |6 PA0 NRST/UPDI/CS
+WD   PA6 2|      |7 PA3 CLK
+CS   PA7 3|      |6 PA0 NRST/UPDI/CS
 MOSI PA1 4|______|5 PA2 MISO // WD
 */
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#define F_CPU 16000000UL
+#define F_CPU 1000000UL
 #include <util/delay.h>
 
-#define PA0_CS PIN0_bm
 #define PA1_MOSI PIN1_bm
-#define PA2_WD PIN2_bm
+#define PA2_MISO PIN2_bm
 #define PA3_CLK PIN3_bm
-#define PA6_LCD PIN6_bm
-#define PA7_INT PIN7_bm
+#define PA6_WD PIN6_bm
+#define PA7_CS PIN7_bm
 
 void GPIOWD(int v) {
 	if (v)
-		PORTA.OUTSET = PA2_WD;
+		PORTA.OUTSET = PA6_WD;
 	else
-		PORTA.OUTCLR = PA2_WD;
+		PORTA.OUTCLR = PA6_WD;
 }
 
 void SPI(uint8_t l, uint8_t a, uint8_t b) {
-	PORTA.OUTCLR = PA0_CS;
+	PORTA.OUTCLR = PA7_CS;
 	SPI0.DATA = a;
 	while ((SPI0.INTFLAGS & SPI_RXCIF_bm) == 0) ;
 	if (l == 2) {
 		SPI0.DATA = b;
 		while ((SPI0.INTFLAGS & SPI_RXCIF_bm) == 0) ;
 	}
-	PORTA.OUTSET = PA0_CS;
+	PORTA.OUTSET = PA7_CS;
 }
 
 void WAIT_100MS() {
@@ -42,39 +41,39 @@ void WAIT_100MS() {
 #include "screenSimple.h"
 
 uint8_t loop = 1;
-ISR(RTC_PIT_vect) {
-	loop = 0;
-	RTC.PITINTFLAGS = RTC_PI_bm;
-}
 
 int main() {
-	PORTA.PIN0CTRL |= PORT_ISC_INPUT_DISABLE_gc;
 	PORTA.PIN1CTRL |= PORT_ISC_INPUT_DISABLE_gc;
-	PORTA.PIN2CTRL |= PORT_ISC_INPUT_DISABLE_gc;
 	PORTA.PIN3CTRL |= PORT_ISC_INPUT_DISABLE_gc;
 	PORTA.PIN6CTRL |= PORT_ISC_INPUT_DISABLE_gc;
 	PORTA.PIN7CTRL |= PORT_ISC_INPUT_DISABLE_gc;
 
-	PORTA.DIRSET = PA1_MOSI | PA3_CLK | PA0_CS;
+	PORTA.DIRSET = (PA1_MOSI | PA3_CLK | PA6_WD | PA7_CS) & ~PA2_MISO;
+
+	SPI0.CTRLB |= SPI_MODE_0_gc; // Select SPI Mode 0
 	SPI0.CTRLA |= SPI_MASTER_bm; // Select master mode
-	SPI0.CTRLA |= SPI_PRESC_DIV4_gc; // Select Prescaler of 2
+	//SPI0.CTRLA |= SPI_PRESC_DIV4_gc; // Select Prescaler of 2
 	SPI0.CTRLA |= SPI_CLK2X_bm; // Enable double clock speed
-	SPI0.CTRLB |= SPI_SSD_bm; // Disable slave select
-	SPI0.CTRLB |= SPI_MODE_3_gc; // Select SPI Mode 3
 	SPI0.CTRLA |= SPI_ENABLE_bm; // Enable SPI
+	SPI0.CTRLB |= SPI_SSD_bm; // Disable slave select
 
-	PORTA.DIRSET |= PA2_WD | PA6_LCD;
+#if 0
+	int i = 100;
+	while (i-- != 1) {
+		GPIOWD(1);
+		PORTA.OUTSET = PA7_CS;
+		_delay_ms(10);
+		GPIOWD(0);
+		PORTA.OUTCLR = PA7_CS;
+		_delay_ms(10);
+	}
+#endif
 
-	PORTA.DIRSET &= ~PA7_INT;
-	RTC.CLKSEL = RTC_CLKSEL_INT1K_gc;				// 1024 Hz from OSCULP32K
-	RTC.CTRLA = RTC_RTCEN_bm;					// enable RTC
-	RTC.PITINTCTRL = RTC_PI_bm;					// enable periodic interrupt
-	RTC.PITCTRLA = RTC_PERIOD_CYC16384_gc | RTC_PITEN_bm;	
-    cli();
-    sei();
-
+	_delay_ms(5000);
 	screenInit_();
+
+	int count = 0;
 	while (loop)
-		screenRect_(0, 0, 128, 128, 64938);
+		screenRect_(0, 0, 128, 128, (count++ % 2) ? 64938 : 0);
 	return 0;
 }
