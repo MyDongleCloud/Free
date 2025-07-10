7030041c20849e10da748c0b9f75cc1e06bc72ae
Gregoire Gentil <gregoire@gentil.com>
2025-07-10 12:55:27 -0700

    Add an extract option in burn script

--- private/burn.sh
+++ private/burn.sh
@@ -2,8 +2,9 @@
 
 helper() {
 echo "*******************************************************"
-echo "Usage for burn.sh [-d disk -i img -h]"
+echo "Usage for burn.sh [-d disk -e -i img -h]"
 echo "d disk:	set /dev/disk"
+echo "e:	extract image"
 echo "i img:	set image to burn"
 echo "h:		Print this usage and exit"
 exit 0
@@ -11,9 +12,11 @@ exit 0
 
 DISK=/dev/sda
 IMG=/work/ai.mydonglecloud/private/img/flasher-m-s.img
-while getopts d:i:h opt; do
+EXTRACT=0
+while getopts d:ei:h opt; do
 	case "$opt" in
 		d) DISK="/dev/${OPTARG}";;
+		e) EXTRACT=1;;
 		i) IMG="${OPTARG}";;
 		h) helper;;
 	esac
@@ -37,5 +40,10 @@ e2fsck -f -p ${DISK}2
 resize2fs ${DISK}2
 e2fsck -f -p ${DISK}2
 mount ${DISK}2 /tmp/2
+if [ $EXTRACT = 1 ]; then
+	unsquashfs -f -d /tmp/2 /tmp/2/fs/mdc.img
+	rm -rf /tmp/2/fs*
+	sync
+fi
 umount ${DISK}*
 umount ${DISK}*
