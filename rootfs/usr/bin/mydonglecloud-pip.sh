b099d11db4f1e8d3ff84d076c15b6ed6ee88f72f
Gregoire Gentil <gregoire@gentil.com>
2025-07-16 15:01:56 -0700

    Add option to chose python version in mydonglecloud-pip script

--- rootfs/usr/bin/mydonglecloud-pip.sh
+++ rootfs/usr/bin/mydonglecloud-pip.sh
@@ -2,27 +2,29 @@
 
 helper() {
 echo "*******************************************************"
-echo "Usage for pip.sh [-f folder -h -l -s]"
+echo "Usage for pip.sh [-f fold -h -l -s -v vers]"
 echo "f fold:	Set folder"
 echo "h:	Print this usage and exit"
 echo "l:	Local"
 echo "s:	Setup"
+echo "v vers:	Set version"
 exit 0
 }
 
 SETUP=0
 VERSION=3.11
-PYTHON=python$VERSION
 LOCAL=0
 FOLDER=/tmp/test
-while getopts f:hls opt; do
+while getopts f:hlsv: opt; do
 	case "$opt" in
 		f) FOLDER="${OPTARG}";;
 		h) helper;;
 		l) LOCAL=1;;
 		s) SETUP=1;;
+		v) VERSION="${OPTARG}";;
 	esac
 done
+PYTHON=python$VERSION
 
 if [ $SETUP = 1 ]; then
 	echo "#####################################"
