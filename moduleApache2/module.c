24f4ff2c205226aa7990c36c4815ca7f7d6f1ac7
Gregoire Gentil <gregoire@gentil.com>
2025-08-01 17:01:48 -0700

    Implement the user authorization in the custom apache2 module which reads user authorization in /disk/admin/.modules/MyDongleCloud/users.json

--- moduleApache2/module.c
+++ moduleApache2/module.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/stat.h>
 #include "util_cookies.h"
 #include "http_log.h"
 #include "apr_strings.h"
@@ -9,61 +10,123 @@
 //Struct
 typedef struct {
 	const char *name;
-} moduleNameConfig;
+	cJSON *authorized;
+} directoryConfig;
 
-//Define
+//Defines
 #undef PRINTF_
 #undef PRINTF
 #define PRINTF_(level, format, ...) {ap_log_error(APLOG_MARK, level, 0, s, format, ##__VA_ARGS__);}
 #define PRINTF(format, ...) PRINTF_(APLOG_INFO, format, ##__VA_ARGS__)
 
-//Private variable
-cJSON *users = NULL;
-
 //Functions
 module AP_MODULE_DECLARE_DATA mydonglecloud_module;
 
-static void usersLoad() {
+static cJSON *usersLoad() {
+	struct stat statTest;
+	if (stat(ADMIN_PATH "MyDongleCloud/users.json", &statTest) != 0 || statTest.st_size == 0)
+		return NULL;
+	int size = statTest.st_size + 16;
+	char *sz = malloc(size);
 	FILE *f = fopen(ADMIN_PATH "MyDongleCloud/users.json", "r");
+	cJSON *users = NULL;
 	if (f) {
-		char sz[1024];
-		fread(sz, sizeof(sz), 1, f);
+		int ret = fread(sz, size, 1, f);
 		users = cJSON_Parse(sz);
 		fclose(f);
 	}
+	free(sz);
+	return users;
 }
 
-static void *createDirConfig(apr_pool_t *p, char *dirspec) {
-	moduleNameConfig *conf = apr_pcalloc(p, sizeof(moduleNameConfig));
-	conf->name = NULL;
-	return conf;
+static void *createDirectoryConfig(apr_pool_t *p, char *dirspec) {
+	directoryConfig *confD = apr_pcalloc(p, sizeof(directoryConfig));
+	confD->name = NULL;
+	confD->authorized = NULL;
+	return confD;
 }
 
 static const char *moduleNameSet(cmd_parms *cmd, void *mconfig, const char *arg) {
 	server_rec *s = cmd->server;
-    moduleNameConfig *conf = (moduleNameConfig *)mconfig;
-    conf->name = arg;
+    directoryConfig *confD = (directoryConfig *)mconfig;
+    confD->name = arg;
 	PRINTF("MDC: Set %s", arg);
     return NULL;
 }
 
-static int authorizationChecker(request_rec *r) {
+static const char *moduleAuthorizedSet(cmd_parms *cmd, void *mconfig, const char *arg) {
+	server_rec *s = cmd->server;
+    directoryConfig *confD = (directoryConfig *)mconfig;
+	PRINTF("MDC: Add user %s for %s", arg, confD->name);
+	if (confD->authorized == NULL)
+		confD->authorized = cJSON_CreateObject();
+	cJSON_AddBoolToObject(confD->authorized, arg, cJSON_True);
+    return NULL;
+}
+
+char *extractCookieValue(const char *cookie, const char *name, struct request_rec *r) {
+    if (cookie == NULL || name == NULL)
+        return NULL;
+    const char *name_start = strstr(cookie, name);
+    if (name_start == NULL)
+        return NULL;
+    if (*(name_start + strlen(name)) != '=')
+        return extractCookieValue(name_start + 1, name, r);
+
+    const char *value_start = name_start + strlen(name) + 1;
+    const char *value_end = strchr(value_start, ';');
+
+    size_t value_len = value_end == NULL ? strlen(value_start) : (value_end - value_start);
+    char *extracted_value = apr_pstrndup(r->pool, value_start, value_len);
+    return extracted_value;
+}
+
+static int authorization(request_rec *r) {
 	server_rec *s = r->server;
-	const char *cookie_header = apr_table_get(r->headers_in, "Cookie");
-    moduleNameConfig *conf = ap_get_module_config(r->per_dir_config, &mydonglecloud_module);
-	PRINTF("MDC: authorizationChecker (%s)", conf->name);
-	return DECLINED;
+    directoryConfig *confD = ap_get_module_config(r->per_dir_config, &mydonglecloud_module);
+	//PRINTF("MDC: authorization1 name:%s", confD->name);
+	if (confD->name == NULL || confD->authorized == NULL)
+		return DECLINED;
+	const char *cookies = apr_table_get(r->headers_in, "Cookie");
+	char *cookieUser = extractCookieValue(cookies, "user", r);
+	//PRINTF("MDC: authorization2 cookieUser:%s", cookieUser);
+	if (cookieUser != NULL) {
+		if (cJSON_HasObjectItem(confD->authorized, "_allusers_") || cJSON_HasObjectItem(confD->authorized, cookieUser)) {
+			char *cookieToken = extractCookieValue(cookies, "token", r);
+			//PRINTF("MDC: authorization3 cookieToken:%s", cookieToken);
+			if (cookieToken != NULL) {
+				cJSON *users = usersLoad(s);
+				if (users != NULL) {
+					cJSON *el = cJSON_GetObjectItem(users, cookieUser);
+					if (el) {
+						cJSON *el2 = cJSON_GetObjectItem(el, "token");
+						if (el2) {
+							char *st = cJSON_GetStringValue(el2);
+							//PRINTF("MDC: authorization4 userToken:%s", st);
+							if (st != NULL && strcmp(cookieToken, st) == 0) {
+								cJSON_Delete(users);
+								return DECLINED;
+							}
+						}
+					}
+					cJSON_Delete(users);
+				}
+			}
+		}
+	}
+	return HTTP_UNAUTHORIZED;
 }
 
 static const command_rec directives[] = {
 	AP_INIT_TAKE1("MyDongleCloudModule", moduleNameSet, NULL, RSRC_CONF | ACCESS_CONF, "MyDongleCloud module name"),
+	AP_INIT_ITERATE("MyDongleCloudModuleAuthorized", moduleAuthorizedSet, NULL, RSRC_CONF | ACCESS_CONF, "Authorized users list for MyDongleCloud module"),
 	{NULL}
 };
 
 static void registerHooks(apr_pool_t *p) {
-	ap_hook_handler(authorizationChecker, NULL, NULL, APR_HOOK_MIDDLE);
+	ap_hook_handler(authorization, NULL, NULL, APR_HOOK_FIRST);
 }
 
 module AP_MODULE_DECLARE_DATA mydonglecloud_module = {
-	STANDARD20_MODULE_STUFF, createDirConfig, NULL, NULL, NULL, directives, registerHooks
+	STANDARD20_MODULE_STUFF, createDirectoryConfig, NULL, NULL, NULL, directives, registerHooks
 };
