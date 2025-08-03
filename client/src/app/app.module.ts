8e6c268bad8d2d008015a85facce777faa627aef
Gregoire Gentil <gregoire@gentil.com>
2025-08-03 12:53:52 -0700

    Add Taiga UI for the client app

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,17 +8,19 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NG_EVENT_PLUGINS } from '@taiga-ui/event-plugins';
+import { TuiRoot } from '@taiga-ui/core';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, TranslateModule.forRoot({
+	imports: [BrowserModule, TuiRoot, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
         useFactory: (createTranslateLoader),
         deps: [HttpClient]
       }
     })],
-	providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+	providers: [NG_EVENT_PLUGINS, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
 	bootstrap: [AppComponent],
 })
 
