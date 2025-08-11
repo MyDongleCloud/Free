af28955d7eea6b7510e812ddc675e5dc3bdb911e
Gregoire Gentil <gregoire@gentil.com>
2025-08-11 13:27:21 -0700

    Clean-up Apache2 config creation code (replacing domain by fqdn) and create the ports.conf file

--- app/apache2.c
+++ app/apache2.c
@@ -6,11 +6,11 @@
 #include "cJSON.h"
 
 //Functions
-static void writePermissions(cJSON *elLocalRanges, char *authorized, FILE *pf) {
+static void writePermissions(cJSON *elLocalRanges, char *authorized, FILE *pfM) {
 //ex: _all_,_dongle_,_localnetwork_,_allusers_,admin,user1
 	if (strstr(authorized, "_all_") != NULL) {
 		char sz[] = "\t\tRequire all granted\n";
-		fwrite(sz, strlen(sz), 1, pf);
+		fwrite(sz, strlen(sz), 1, pfM);
 	} else {
 		char *authorized_ = strdup(authorized);
 		char *token = strtok(authorized_, ",");
@@ -20,13 +20,13 @@ static void writePermissions(cJSON *elLocalRanges, char *authorized, FILE *pf) {
 		while (token != NULL) {
 			if (strcmp(token, "_dongle_") == 0) {
 				char sz[] = "\t\tRequire local\n";
-				fwrite(sz, strlen(sz), 1, pf);
+				fwrite(sz, strlen(sz), 1, pfM);
 			} else if (strcmp(token, "_localnetwork_") == 0) {
 				cJSON *item = NULL;
 				cJSON_ArrayForEach(item, elLocalRanges) {
 					char sz[256];
 					sprintf(sz, "\t\tRequire ip %s\n", item->valuestring);
-					fwrite(sz, strlen(sz), 1, pf);
+					fwrite(sz, strlen(sz), 1, pfM);
 				}
 			} else {
 				if (strlen(list) == 0)
@@ -40,55 +40,67 @@ static void writePermissions(cJSON *elLocalRanges, char *authorized, FILE *pf) {
 		free(authorized_);
 		if (strlen(list) != 0) {
 			strcat(list, "\n");
-			fwrite(list, strlen(list), 1, pf);
+			fwrite(list, strlen(list), 1, pfM);
 		}
 		if (requireNb > 1) {
 				char sz[] = "\t\tSatisfy any\n";
-				fwrite(sz, strlen(sz), 1, pf);
+				fwrite(sz, strlen(sz), 1, pfM);
 		}
 	}
 }
 
-static void writeSymlinks(int b, FILE *pf) {
+static void writeSymlinks(int b, FILE *pfM) {
 	char sz[256];
 	sprintf(sz, "\t\tOptions %sFollowSymLinks\n", b ? "+" : "-");
-	fwrite(sz, strlen(sz), 1, pf);
+	fwrite(sz, strlen(sz), 1, pfM);
 }
 
-static void writeIndexes(int b, FILE *pf) {
+static void writeIndexes(int b, FILE *pfM) {
 	char sz[256];
 	sprintf(sz, "\t\tOptions %sIndexes\n", b ? "+" : "-");
-	fwrite(sz, strlen(sz), 1, pf);
+	fwrite(sz, strlen(sz), 1, pfM);
 }
 
-static void writeDirectoryIndex(char *s, FILE *pf) {
+static void writeDirectoryIndex(char *s, FILE *pfM) {
 	char sz[256];
 	sprintf(sz, "\t\tDirectoryIndex %s\n", s);
-	fwrite(sz, strlen(sz), 1, pf);
+	fwrite(sz, strlen(sz), 1, pfM);
 }
 
-static void writeLog(char *name, FILE *pf) {
+static void writeLog(char *name, FILE *pfM) {
 	char sz[256];
 	sprintf(sz, "/disk/admin/.log/%s", name);
 	mkdir(sz, 755);
 	sprintf(sz, "\tCustomLog /disk/admin/.log/%s/web.log combined\n", name);
-	fwrite(sz, strlen(sz), 1, pf);
+	fwrite(sz, strlen(sz), 1, pfM);
 }
 
 void reloadApache2Conf() {
-#if !defined(DESKTOP) && !defined(WEB)
+#ifndef DESKTOP
+	PRINTF("Apache2: Reloading\n");
 	system("sudo /usr/bin/systemctl reload apache2");
-	PRINTF("Apache2: Reloaded\n");
 #endif
 }
 
-void buildApache2Conf(cJSON *modulesDefault, cJSON *modules, char *domain) {
+void buildApache2Conf(cJSON *modulesDefault, cJSON *modules, char *fqdn) {
+	PRINTF("Apache2: Creation of configs\n");
 #ifdef DESKTOP
-	FILE *pf = fopen("/tmp/main.conf", "w");
+	FILE *pfP = fopen("/tmp/ports.conf", "w");
 #else
-	FILE *pf = fopen(ADMIN_PATH "Apache2/main.conf", "w");
+	FILE *pfP = fopen(ADMIN_PATH "Apache2/ports.conf", "w");
 #endif
 	char sz[2048];
+	strcpy(sz, "\
+Listen 80\n\
+<IfModule ssl_module>\n\
+	Listen 443\n\
+</IfModule>\n");
+	fwrite(sz, strlen(sz), 1, pfP);
+#ifdef DESKTOP
+	FILE *pfM = fopen("/tmp/main.conf", "w");
+#else
+	FILE *pfM = fopen(ADMIN_PATH "Apache2/main.conf", "w");
+#endif
 	strcpy(sz, "LoadModule mydonglecloud_module /usr/local/modules/Apache2/mod_mydonglecloud.so\n\
 \n\
 <Macro Macro_Redirect>\n\
@@ -105,7 +117,7 @@ void buildApache2Conf(cJSON *modulesDefault, cJSON *modules, char *domain) {
     SSLCertificateFile /disk/admin/.modules/Apache2/fullchain.pem\n\
     SSLCertificateKeyFile /disk/admin/.modules/Apache2/privkey.pem\n\
 </Macro>\n\n");
-	fwrite(sz, strlen(sz), 1, pf);
+	fwrite(sz, strlen(sz), 1, pfM);
 	cJSON *elLocalRanges = cJSON_GetObjectItem(cJSON_GetObjectItem(modulesDefault, "Apache2"), "localRanges");
 
 	for (int i = 0; i < cJSON_GetArraySize(modulesDefault); i++) {
@@ -121,7 +133,7 @@ void buildApache2Conf(cJSON *modulesDefault, cJSON *modules, char *domain) {
 <Macro Macro_%s>\n\
 	MyDongleCloudModule %s\n\
 	RewriteEngine On\n", elModule->string, elModule->string);
-			fwrite(sz, strlen(sz), 1, pf);
+			fwrite(sz, strlen(sz), 1, pfM);
 
 			cJSON *elEnabled = cJSON_GetObjectItem(elModule, "enabled");
 			if (cJSON_HasObjectItem(elModule2, "enabled"))
@@ -139,30 +151,38 @@ void buildApache2Conf(cJSON *modulesDefault, cJSON *modules, char *domain) {
 	ProxyPreserveHost on\n\
 	ProxyPass / http://localhost:%d/\n\
 	ProxyPassReverse / http://localhost:%d/\n\t<Proxy *>\n", port, port);
-					fwrite(sz, strlen(sz), 1, pf);
+					fwrite(sz, strlen(sz), 1, pfM);
 				} else {
 					sprintf(sz, "\tDocumentRoot %s\n\t<Directory %s>\n", path, path);
-					fwrite(sz, strlen(sz), 1, pf); 
+					fwrite(sz, strlen(sz), 1, pfM);
 				}
-				writePermissions(elLocalRanges, authorized, pf);
+				if (cJSON_HasObjectItem(elModule, "addLineInDirectory")) {
+					sprintf(sz, "\t\t%s\n", cJSON_GetStringValue(cJSON_GetObjectItem(elModule, "addLineInDirectory")));
+					fwrite(sz, strlen(sz), 1, pfM);
+				}
+				if (cJSON_HasObjectItem(elModule2, "addLineInDirectory")) {
+					sprintf(sz, "\t\t%s\n", cJSON_GetStringValue(cJSON_GetObjectItem(elModule2, "addLineInDirectory")));
+					fwrite(sz, strlen(sz), 1, pfM);
+				}
+				writePermissions(elLocalRanges, authorized, pfM);
 				if (cJSON_HasObjectItem(elModule2, "DirectoryIndex"))
-					writeDirectoryIndex(cJSON_GetStringValue(cJSON_GetObjectItem(elModule2, "DirectoryIndex")), pf);
+					writeDirectoryIndex(cJSON_GetStringValue(cJSON_GetObjectItem(elModule2, "DirectoryIndex")), pfM);
 				else if (cJSON_HasObjectItem(elModule, "DirectoryIndex"))
-					writeDirectoryIndex(cJSON_GetStringValue(cJSON_GetObjectItem(elModule, "DirectoryIndex")), pf);
+					writeDirectoryIndex(cJSON_GetStringValue(cJSON_GetObjectItem(elModule, "DirectoryIndex")), pfM);
 				if (cJSON_HasObjectItem(elModule2, "FollowSymLinks"))
-					writeSymlinks(cJSON_IsTrue(cJSON_GetObjectItem(elModule2, "FollowSymLinks")), pf);
+					writeSymlinks(cJSON_IsTrue(cJSON_GetObjectItem(elModule2, "FollowSymLinks")), pfM);
 				else if (cJSON_GetObjectItem(elModule, "FollowSymLinks"))
-					writeSymlinks(cJSON_IsTrue(cJSON_GetObjectItem(elModule, "FollowSymLinks")), pf);
+					writeSymlinks(cJSON_IsTrue(cJSON_GetObjectItem(elModule, "FollowSymLinks")), pfM);
 				if (cJSON_HasObjectItem(elModule2, "Indexes"))
-					writeIndexes(cJSON_IsTrue(cJSON_GetObjectItem(elModule2, "Indexes")), pf);
+					writeIndexes(cJSON_IsTrue(cJSON_GetObjectItem(elModule2, "Indexes")), pfM);
 				else if (cJSON_GetObjectItem(elModule, "Indexes"))
-					writeIndexes(cJSON_IsTrue(cJSON_GetObjectItem(elModule, "Indexes")), pf);
+					writeIndexes(cJSON_IsTrue(cJSON_GetObjectItem(elModule, "Indexes")), pfM);
 
 				if (cJSON_HasObjectItem(elModule, "reverseProxy"))
 					strcpy(sz, "\t</Proxy>\n");
 				else
 					strcpy(sz, "\t</Directory>\n");
-				fwrite(sz, strlen(sz), 1, pf);
+				fwrite(sz, strlen(sz), 1, pfM);
 
 				if (strcmp(elModule->string, "Apache2") == 0) {
 					for (int ii = 0; ii < cJSON_GetArraySize(modulesDefault); ii++) {
@@ -172,71 +192,67 @@ void buildApache2Conf(cJSON *modulesDefault, cJSON *modules, char *domain) {
 							port_ = (int)cJSON_GetNumberValue(cJSON_GetObjectItem(el_Module, "fallbackPort"));
 						if (port_ > 0) {
 							sprintf(sz, "\
-	RewriteRule ^/(MyDongleCloud|m)/%s.* http://%s.%s [NC,L]\n\
     RewriteCond %%{HTTP_HOST} ^(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})$\n\
-	RewriteRule ^/(MyDongleCloud|m)/%s.* http://%%{HTTP_HOST}:%d [NC,L]\n", el_Module->string, el_Module->string, domain, el_Module->string, port_);
-							fwrite(sz, strlen(sz), 1, pf);
+	RewriteRule ^/(MyDongleCloud|m)/%s.* http://%%{HTTP_HOST}:%d [NC,L]\n\
+	RewriteRule ^/(MyDongleCloud|m)/%s.* http://%s.%s [NC,L]\n", el_Module->string, port_, el_Module->string, el_Module->string, fqdn);
+							fwrite(sz, strlen(sz), 1, pfM);
 						}
 					}
 					strcpy(sz, "\n\tAlias /MyDongleCloud /usr/local/modules/Apache2/pages\n\tAlias /m /usr/local/modules/Apache2/pages\n");
-					fwrite(sz, strlen(sz), 1, pf);
+					fwrite(sz, strlen(sz), 1, pfM);
 				}
 				strcpy(sz, "\tUse Macro_Redirect\n");
-				fwrite(sz, strlen(sz), 1, pf);
+				fwrite(sz, strlen(sz), 1, pfM);
 			} else {
-				sprintf(sz, "\tRewriteRule ^/.*$ http://%s/MyDongleCloud/disabled.php?m=%s\n\n", domain, elModule->string);
-				fwrite(sz, strlen(sz), 1, pf);
+				sprintf(sz, "\tRewriteRule ^/.*$ http://%s/MyDongleCloud/disabled.php?m=%s\n\n", fqdn, elModule->string);
+				fwrite(sz, strlen(sz), 1, pfM);
 			}
-			writeLog(elModule->string, pf);
+			writeLog(elModule->string, pfM);
 			sprintf(sz, "</Macro>\n");
-			fwrite(sz, strlen(sz), 1, pf);
+			fwrite(sz, strlen(sz), 1, pfM);
 
 			sprintf(sz, "<VirtualHost *:80>\n");
-			fwrite(sz, strlen(sz), 1, pf);
-			if (domain != NULL) {
+			fwrite(sz, strlen(sz), 1, pfM);
+			if (fqdn != NULL) {
 				if (strcmp(elModule->string, "Apache2") == 0)
-					sprintf(sz, "\tServerName %s\n", domain);
+					sprintf(sz, "\tServerName %s\n", fqdn);
 				else
-					sprintf(sz, "\tServerName %s.%s\n", elModule->string, domain);
-				fwrite(sz, strlen(sz), 1, pf);
+					sprintf(sz, "\tServerName %s.%s\n", elModule->string, fqdn);
+				fwrite(sz, strlen(sz), 1, pfM);
 				if (cJSON_HasObjectItem(elModule, "alias")) {
-					sprintf(sz, "\tServerAlias %s.%s\n", cJSON_GetStringValue(cJSON_GetObjectItem(elModule, "alias")), domain);
-					fwrite(sz, strlen(sz), 1, pf);
+					sprintf(sz, "\tServerAlias %s.%s\n", cJSON_GetStringValue(cJSON_GetObjectItem(elModule, "alias")), fqdn);
+					fwrite(sz, strlen(sz), 1, pfM);
 				}
 			}
 			sprintf(sz, "\tUse Macro_%s\n</VirtualHost>\n", elModule->string);
-			fwrite(sz, strlen(sz), 1, pf);
+			fwrite(sz, strlen(sz), 1, pfM);
 
 			sprintf(sz, "<IfModule mod_ssl.c>\n\t<VirtualHost *:443>\n");
-			fwrite(sz, strlen(sz), 1, pf);
-			if (domain != NULL) {
+			fwrite(sz, strlen(sz), 1, pfM);
+			if (fqdn != NULL) {
 				if (strcmp(elModule->string, "Apache2") == 0)
-					sprintf(sz, "\t\tServerName %s\n", domain);
+					sprintf(sz, "\t\tServerName %s\n", fqdn);
 				else
-					sprintf(sz, "\t\tServerName %s.%s\n", elModule->string, domain);
-				fwrite(sz, strlen(sz), 1, pf);
+					sprintf(sz, "\t\tServerName %s.%s\n", elModule->string, fqdn);
+				fwrite(sz, strlen(sz), 1, pfM);
 				if (cJSON_HasObjectItem(elModule, "alias")) {
-					sprintf(sz, "\t\tServerAlias %s.%s\n", cJSON_GetStringValue(cJSON_GetObjectItem(elModule, "alias")), domain);
-					fwrite(sz, strlen(sz), 1, pf);
+					sprintf(sz, "\t\tServerAlias %s.%s\n", cJSON_GetStringValue(cJSON_GetObjectItem(elModule, "alias")), fqdn);
+					fwrite(sz, strlen(sz), 1, pfM);
 				}
 			}
 			sprintf(sz, "\t\tUse Macro_%s\n\t\tUse Macro_SSL\n\t</VirtualHost>\n</IfModule>\n", elModule->string);
-			fwrite(sz, strlen(sz), 1, pf);
+			fwrite(sz, strlen(sz), 1, pfM);
 			int fallbackPort = (int)cJSON_GetNumberValue(cJSON_GetObjectItem(elModule, "fallbackPort"));
 			if (fallbackPort > 0) {
-				sprintf(sz, "<VirtualHost *:%d>\n", fallbackPort);
-				fwrite(sz, strlen(sz), 1, pf);
-				if (domain != NULL) {
-					sprintf(sz, "\tServerName %s\n", domain);
-					fwrite(sz, strlen(sz), 1, pf);
-				}
-				sprintf(sz, "\tUse Macro_%s\n</VirtualHost>\n", elModule->string);
-				fwrite(sz, strlen(sz), 1, pf);
+				sprintf(sz, "<VirtualHost *:%d>\n\tUse Macro_%s\n</VirtualHost>\n", fallbackPort, elModule->string);
+				fwrite(sz, strlen(sz), 1, pfM);
+				sprintf(sz, "Listen %d\n", fallbackPort);
+				fwrite(sz, strlen(sz), 1, pfP);
 			}
 			strcpy(sz, "\n\n");
-			fwrite(sz, strlen(sz), 1, pf);
+			fwrite(sz, strlen(sz), 1, pfM);
 		}
 	}
-	fclose(pf);
-	PRINTF("Apache2: Creation of main.conf\n");
+	fclose(pfP);
+	fclose(pfM);
 }
--- private/install.sh
+++ private/install.sh
@@ -188,6 +188,7 @@ echo "Apache2"
 echo "################################"
 apt-get -y install apache2
 rm -f /etc/apache2/sites-enabled/*
+rm -f /etc/apache2/ports.conf
 
 echo "################################"
 echo "Roundcube"
