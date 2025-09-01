6928187c5a11fd64195ea7025d9490541a92df26
Gregoire Gentil <gregoire@gentil.com>
2025-09-01 14:39:33 +0200

    Improve dtb-pro script by adding a local option

--- kernel/dtb-pro.sh
+++ kernel/dtb-pro.sh
@@ -1,6 +1,26 @@
 #!/bin/sh
 
-dtc -@ -I dts -O dtb -o /boot/firmware/overlays/mydonglecloud.dtbo mydonglecloud-overlay.dts
-dtc -@ -I dts -O dtb -o /boot/firmware/overlays/buttons.dtbo buttons-overlay.dts
-dtc -@ -I dts -O dtb -o /boot/firmware/overlays/leds.dtbo leds-overlay.dts
-dtc -@ -I dts -O dtb -o /boot/firmware/overlays/st7735s.dtbo st7735s-overlay.dts
+helper() {
+echo "*******************************************************"
+echo "usage of dtb-pro [-h -l]"
+echo "h:	Print this usage and exit"
+echo "l:	Do local"
+exit 0
+}
+
+PRE="/boot/firmware"
+while getopts hl opt; do
+	case "$opt" in
+		h) helper;;
+		l) PRE="/tmp/1";;
+	esac
+done
+
+if [ ! -f mydonglecloud-overlay.dts ]; then
+	echo "Not in the right directory"
+	exit 0
+fi
+dtc -@ -I dts -O dtb -o ${PRE}/overlays/mydonglecloud.dtbo mydonglecloud-overlay.dts
+dtc -@ -I dts -O dtb -o ${PRE}/overlays/buttons.dtbo buttons-overlay.dts
+dtc -@ -I dts -O dtb -o ${PRE}/overlays/leds.dtbo leds-overlay.dts
+dtc -@ -I dts -O dtb -o ${PRE}/overlays/st7735s.dtbo st7735s-overlay.dts
