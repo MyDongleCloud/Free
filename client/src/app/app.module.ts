31628bc20bd90640fce86c1d87746dd49c1a1596
Gregoire Gentil <gregoire@gentil.com>
2025-09-14 23:24:42 -0700

    Modify, fix, clean-up (though it was working) translation with ngx-translate 17 in client app

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,33 +1,31 @@
-import { NgModule, importProvidersFrom } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { provideHttpClient } from '@angular/common/http';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { provideTranslateHttpLoader, TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-	providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-	provideHttpClient(),
-	provideTranslateHttpLoader(),
-	importProvidersFrom(
+	imports: [
+		BrowserModule,
+		IonicModule.forRoot(),
+		AppRoutingModule,
 		TranslateModule.forRoot({
-			defaultLanguage: "en",
-			loader: {
-				provide: TranslateLoader,
-				useFactory: HttpLoaderFactory
-			}
-		})
-	),
-],
-  bootstrap: [AppComponent],
+			loader: provideTranslateHttpLoader({
+				prefix: "/assets/i18n/",
+				suffix: ".json",
+			}),
+			fallbackLang: "en"
+		})],
+	providers: [
+		{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+		provideHttpClient()
+	],
+	bootstrap: [AppComponent],
 })
-export class AppModule {}
 
-export function HttpLoaderFactory(): TranslateLoader {
-  return new TranslateHttpLoader();
-}
+export class AppModule {}
