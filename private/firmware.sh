0bcf3e794314337fe8ecde1e721028de2c84d458
Gregoire Gentil <gregoire@gentil.com>
2025-09-21 22:48:04 -0700

    Fix small issues in install and firmware scripts

--- private/firmware.sh
+++ private/firmware.sh
@@ -65,7 +65,7 @@ if [ -f /tmp/mdc${POSTNAME}.img ]; then
 	echo "No creation as /tmp/mdc${POSTNAME}.img already exists"
 else
 	tar -xjpf /work/ai.inout/private/img/modules-artik.tbz2 -C ${ROOTFS}/lib/modules/
-	rm -rf ${ROOTFS}/home/mdc/app/ ${ROOTFS}/home/mdc/moduleApache2/ ${ROOTFS}/home/mdc/pam/
+	rm -rf ${ROOTFS}/home/mdc/app/ ${ROOTFS}/home/mdc/auth/ ${ROOTFS}/home/mdc/moduleApache2/ ${ROOTFS}/home/mdc/pam/
 	cp -a ../app/ ../moduleApache2/ ../pam/ ../auth/ ${ROOTFS}/home/mdc/
 	chroot ${ROOTFS} sh -c 'cd /home/mdc/app && make clean && make'
 	chroot ${ROOTFS} sh -c 'cd /home/mdc/moduleApache2 && make clean && make'
