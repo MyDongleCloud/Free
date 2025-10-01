4a8f7a32d31d474cb2733a9b40dcec8951d3c7d7
Gregoire Gentil <gregoire@gentil.com>
2025-09-30 20:50:18 -0700

    Add script to reset MySQL password in dongle image

---
+++ rootfs/usr/local/modules/mysql/mysql.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+helper() {
+echo "*******************************************************"
+echo "Usage for mysql [-h -r]"
+echo "h:	Print this usage and exit"
+echo "r:	Reset root password of mysql"
+exit 0
+}
+
+if [ "m`id -u`" != "m0" ]; then
+	echo "You need to be root"
+	exit 0
+fi
+
+RESET=0
+while getopts hr opt
+do
+	case "$opt" in
+		h) helper;;
+		r) RESET=1;;
+	esac
+done
+
+if [ $RESET != 1 ]; then
+	exit 0
+fi
+
+PASSWORD=$(tr -dc 'A-Za-z0-9!@#$%^&*()_+' < /dev/urandom | head -c 16)
+systemctl stop mysql
+mkdir -p /var/run/mysqld
+chown mysql:mysql /var/run/mysqld
+mysqld_safe --skip-grant-tables --skip-networking &
+TIMEOUT=30
+echo "30 seconds to watch mysql starting..."
+while [ $TIMEOUT -gt 0 ]; do
+    sleep 1
+    TIMEOUT=$((TIMEOUT - 1))
+    [ $TIMEOUT -eq 0 ] && echo "Timeout waiting for MySQL" && exit 1
+	mysql -u root -e "SELECT 1" 2>&1 > /dev/null
+	if [ $? = 0 ]; then
+		break
+	fi
+done
+mysql -u root <<-EOF
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$PASSWORD';
+DROP DATABASE IF EXISTS test;
+FLUSH PRIVILEGES;
+EOF
+mysqladmin -u root -p"$PASSWORD" shutdown
+systemctl start mysql
+echo "[client]\nhost=localhost\nuser=root\npassword=${PASSWORD}" > /disk/admin/.modules/mysql/my.cnf
+chown admin:admin /disk/admin/.modules/mysql/my.cnf
