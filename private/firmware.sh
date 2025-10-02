f8b0d380b33411d646f1801eb222f3c6e7824904
Gregoire Gentil <gregoire@gentil.com>
2025-10-01 21:23:33 -0700

    Include only the relevant boot files for the Dongle Std

--- private/firmware.sh
+++ private/firmware.sh
@@ -52,7 +52,7 @@ if [ $NATIVE = 1 ]; then
 	mount ${DISK}1 /tmp/1
 	mount ${DISK}2 /tmp/2
 	cd /tmp/1
-	zip -q -r ${PP}/img/partition1.zip ./bcm2712-rpi-5-b.dtb ./bcm2712-rpi-cm5-cm5io.dtb ./cmdline.txt ./config.txt ./kernel_2712.img ./overlays/overlay_map.dtb ./overlays/bcm2712d0.dtbo ./overlays/dwc2.dtbo ./overlays/mydonglecloud.dtbo ./overlays/st7735s.dtbo ./overlays/buttons.dtbo ./overlays/leds.dtbo ./overlays/uart0-pi5.dtbo ./overlays/uart2-pi5.dtbo
+	zip -q -r ${PP}/img/partition1.zip ./bcm2712-rpi-5-b.dtb ./bcm2712-rpi-cm5-cm5io.dtb ./cmdline.txt ./config.txt ./kernel_2712.img ./overlays/overlay_map.dtb ./overlays/bcm2712d0.dtbo ./overlays/dwc2.dtbo ./overlays/mydonglecloud.dtbo ./overlays/st7735s.dtbo ./overlays/buttons.dtbo ./overlays/leds.dtbo ./overlays/uart0-pi5.dtbo ./overlays/uart2-pi5.dtbo ./Image ./mydonglecd.dtb
 	cd ${PP}
 	ROOTFS=/tmp/2
 else
