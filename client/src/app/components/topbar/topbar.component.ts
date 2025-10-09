c0cda2596a4e6905d8c64b542ec2b6a60c8d3646
Gregoire Gentil <gregoire@gentil.com>
2025-10-09 13:43:06 -0700

    Add a topbar component to client app

---
+++ client/src/app/components/topbar/topbar.component.ts
@@ -0,0 +1,53 @@
+import { Component, Input, ElementRef, Renderer2, AfterViewInit, OnDestroy } from '@angular/core';
+import { Global } from '../../env';
+
+@Component({
+	selector: 'app-topbar',
+	templateUrl: './topbar.component.html',
+	styleUrls: ['./topbar.component.scss'],
+	standalone: false
+})
+
+export class TopbarComponent implements AfterViewInit, OnDestroy {
+@Input() title: string = "";
+@Input() nameP: string = "";
+private documentClickListener: (() => void) | null = null;
+showUserMenu:boolean = false;
+initials: string = "";
+
+constructor(public global: Global, private elRef: ElementRef,private renderer: Renderer2) {}
+
+toggleUserMenu() {
+	this.showUserMenu = !this.showUserMenu;
+}
+
+
+getInitials() {
+	const st = this.global.session?.user?.name ?? "";
+	const nameParts = st.trim().split(" ");
+	const initials = nameParts.map(name => name.charAt(0)).join("").toUpperCase();
+	return initials;
+}
+
+ngAfterViewInit() {
+	this.initials = this.getInitials();
+	this.documentClickListener = this.renderer.listen("document", "click", (event: Event) => {
+		const userMenuDropdown = this.elRef.nativeElement.querySelector(".user-menu-dropdown");
+		const userMenuButton = this.elRef.nativeElement.querySelector(".user-menu-button");
+		if (this.showUserMenu && userMenuDropdown && userMenuButton && !userMenuDropdown.contains(event.target) && !userMenuButton.contains(event.target))
+			this.showUserMenu = false;
+	});
+}
+
+ngOnDestroy() {
+	if (this.documentClickListener) {
+		this.documentClickListener();
+		this.documentClickListener = null;
+	}
+}
+
+extract() {
+	window.open((document.getElementById("iframeWrapper") as HTMLIFrameElement).src, "_blank");
+}
+
+}
