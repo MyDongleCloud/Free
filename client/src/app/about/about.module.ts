52f07d03ef2ef499a0cd272b59a4a743709d2238
Gregoire Gentil <gregoire@gentil.com>
2025-10-09 14:18:24 -0700

    Redesign about page in client app

--- client/src/app/about/about.module.ts
+++ client/src/app/about/about.module.ts
@@ -2,16 +2,17 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
+import { ComponentsModule } from '../components/components.module';
 import { FormsModule } from '@angular/forms';
 import { AboutRoutingModule } from './about-routing.module';
 import { About } from './about.page';
-import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
 	imports: [
 		CommonModule,
 		IonicModule,
 		TranslateModule,
+		ComponentsModule,
 		FormsModule,
 		ComponentsModule,
 		AboutRoutingModule
