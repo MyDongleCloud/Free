acb861a5caa1e76553d924d4ba7353ff17e2984f
Gregoire Gentil <gregoire@gentil.com>
2025-10-09 13:46:07 -0700

    Redesign dongle page in client app

--- client/src/app/dongle/dongle.module.ts
+++ client/src/app/dongle/dongle.module.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
+import { ComponentsModule } from '../components/components.module';
 import { FormsModule } from '@angular/forms';
 import { DongleRoutingModule } from './dongle-routing.module';
 import { Dongle } from './dongle.page';
-import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
 	imports: [
 		CommonModule,
 		IonicModule,
 		TranslateModule,
+		ComponentsModule,
 		FormsModule,
 		ComponentsModule,
 		DongleRoutingModule
