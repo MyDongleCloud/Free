475d1e6af85b445480e85698df587842019f16d1
Gregoire Gentil <gregoire@gentil.com>
2025-10-09 13:43:57 -0700

    Make available the new components in client app

--- client/src/app/components/components.module.ts
+++ client/src/app/components/components.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { LongPressDirective } from './longPress/longPress.directive';
+import { IonicModule } from '@ionic/angular';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TopbarComponent } from './topbar/topbar.component';
 
 @NgModule({
-	declarations: [LongPressDirective],
-	exports: [LongPressDirective]
+	declarations: [LongPressDirective, SidebarComponent, TopbarComponent],
+	exports: [LongPressDirective, SidebarComponent, TopbarComponent],
+	imports: [IonicModule]
 })
 export class ComponentsModule {}
