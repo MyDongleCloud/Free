2fb433b93b551fb994260e9dc31efa6042810a6f
Gregoire Gentil <gregoire@gentil.com>
2025-10-09 13:47:00 -0700

    Redesign permissions page in client app

--- client/src/app/permissions/permissions.module.ts
+++ client/src/app/permissions/permissions.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
+import { ComponentsModule } from '../components/components.module';
 import { PermissionsRoutingModule } from './permissions-routing.module';
 import { Permissions } from './permissions.page';
 import { FormsModule } from '@angular/forms';
@@ -12,6 +13,7 @@ import { FormsModule } from '@angular/forms';
 		IonicModule,
 		FormsModule,
 		TranslateModule,
+		ComponentsModule,
 		PermissionsRoutingModule
 	],
 	declarations: [Permissions]
