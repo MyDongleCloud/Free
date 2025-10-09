8dc29e440f68c5b7c5e93ffb6c4f6773f3ff63cf
Gregoire Gentil <gregoire@gentil.com>
2025-10-09 13:42:35 -0700

    Add a sidebar component to client app

---
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,33 @@
+import { Component, ElementRef, Renderer2, AfterViewInit, OnDestroy } from '@angular/core';
+import { Global } from '../../env';
+
+@Component({
+	selector: 'app-sidebar',
+	templateUrl: './sidebar.component.html',
+	styleUrls: ['./sidebar.component.scss'],
+	standalone: false
+})
+
+export class SidebarComponent implements AfterViewInit, OnDestroy {
+showMobileMenu: boolean = false;
+private documentClickListener: (() => void) | null = null;
+
+constructor(public global: Global, private elRef: ElementRef,private renderer: Renderer2) {}
+
+ngAfterViewInit() {
+	this.documentClickListener = this.renderer.listen('document', 'click', (event: Event) => {
+		const userMobileMenu = this.elRef.nativeElement.querySelector(".user-mobile-menu");
+		const userMobileButton = this.elRef.nativeElement.querySelector(".user-mobile-button");
+		if (this.showMobileMenu && userMobileMenu && userMobileButton && !userMobileMenu.contains(event.target) && !userMobileButton.contains(event.target))
+			this.showMobileMenu = false;
+	});
+}
+
+ngOnDestroy() {
+	if (this.documentClickListener) {
+		this.documentClickListener();
+		this.documentClickListener = null;
+	}
+}
+
+}
