af64483e396f119efce964c5dcd9eab3adcf5a40
Gregoire Gentil <gregoire@gentil.com>
2025-10-14 14:51:16 -0700

    Clean up and improve test file for Better Auth backend

--- auth/test.sh
+++ auth/test.sh
@@ -46,48 +46,52 @@ done
 
 if [ $CREATE = 1 ]; then
 	echo "############### Create"
-	RET_CREATE=`curl -s -X POST http://localhost:8091/MyDongleCloud/Auth/sign-up/email -H "Content-Type: application/json" -d '{"email":"john.doe@example.com", "name":"John Doe", "password":"demodemo"}' -c /tmp/cookie.txt`
-	echo $RET_CREATE
+	RET_CREATE=`curl -sS --fail -X POST http://localhost:8091/MyDongleCloud/Auth/sign-up/email -H "Content-Type: application/json" -d '{"email":"john.doe@example.com", "name":"John Doe", "password":"demodemo"}' -c /tmp/cookie.txt`
+	echo $RET_CREATE | jq
 fi
 if [ $UPDATE = 1 ]; then
 	echo "############### Update"
-	RET_UPDATE=`curl -s -b /tmp/cookie.txt -X POST http://localhost:8091/MyDongleCloud/Auth/update-user -H "Content-Type: application/json" -d '{"username":"admin"}' -c /tmp/cookie.txt`
-	echo $RET_UPDATE
+	RET_UPDATE=`curl -sS --fail -b /tmp/cookie.txt -X POST http://localhost:8091/MyDongleCloud/Auth/update-user -H "Content-Type: application/json" -d '{"username":"admin"}' -c /tmp/cookie.txt`
+	echo $RET_UPDATE | jq
 fi
 if [ $SIGNIN = 1 ]; then
-	RET_SIGNIN=`curl -s -X POST http://localhost:8091/MyDongleCloud/Auth/sign-in/username -H "Content-Type: application/json" -d '{"username":"admin", "password":""demodemo"}' -c /tmp/cookie.txt`
-	echo $RET_SIGNIN
+	RET_SIGNIN=`curl -sS --fail -X POST http://localhost:8091/MyDongleCloud/Auth/sign-in/email -H "Content-Type: application/json" -d '{"email":"john.doe@example.com", "password":"demodemo"}' -c /tmp/cookie.txt`
+	echo $RET_SIGNIN | jq
+fi
+if [ $SIGNIN = 2 ]; then
+	RET_SIGNIN=`curl -sS --fail -X POST http://localhost:8091/MyDongleCloud/Auth/sign-in/username -H "Content-Type: application/json" -d '{"username":"admin", "password":"demodemo"}' -c /tmp/cookie.txt`
+	echo $RET_SIGNIN | jq
 fi
 if [ $LOGIN = 1 ]; then
 	echo "############### Login"
-	RET_LOGIN=`curl -s -b /tmp/cookie.txt http://localhost:8091/MyDongleCloud/Auth/get-session -H "Content-Type: application/json"`
-	echo $RET_LOGIN
+	RET_LOGIN=`curl -sS --fail -b /tmp/cookie.txt http://localhost:8091/MyDongleCloud/Auth/get -sS --failession -H "Content-Type: application/json"`
+	echo $RET_LOGIN | jq
 fi
 if [ $MAGIC = 1 ]; then
 	echo "############### Magic"
-	RET_MAGIC=`curl -s -X POST http://localhost:8091/MyDongleCloud/Auth/sign-in/magic-link -H "Content-Type: application/json" -d '{"email":"john.doe@example.com", "callbackURL":"http://localhost:8100" }'`
-	echo $RET_MAGIC
+	RET_MAGIC=`curl -sS --fail -X POST http://localhost:8091/MyDongleCloud/Auth/sign-in/magic-link -H "Content-Type: application/json" -d '{"email":"john.doe@example.com", "callbackURL":"http://localhost:8100" }'`
+	echo $RET_MAGIC | jq
 fi
 if [ $TOKEN = 1 ]; then
 	echo "############### Token"
-	RET_JWKS=`curl -s http://localhost:8091/MyDongleCloud/Auth/jwks -H "Content-Type: application/json"`
+	RET_JWKS=`curl -sS --fail http://localhost:8091/MyDongleCloud/Auth/jwks -H "Content-Type: application/json"`
 	#echo $RET_JWKS
-	RET_TOKEN=`curl -s -b /tmp/cookie.txt http://localhost:8091/MyDongleCloud/Auth/token -H "Content-Type: application/json"`
+	RET_TOKEN=`curl -sS --fail -b /tmp/cookie.txt http://localhost:8091/MyDongleCloud/Auth/token -H "Content-Type: application/json"`
 	echo "$RET_TOKEN" | jq -R 'split(".") | .[1] | @base64d | fromjson'
 fi
 if [ $REVOKE = 1 ]; then
 	echo "############### Revoke"
 	TOKEN=`echo $RET_LOGIN | jq -r '.session.token'`
-	RET_REVOKE=`curl -s -b /tmp/cookie.txt -X POST http://localhost:8091/MyDongleCloud/Auth/revoke-session -H "Content-Type: application/json" -d "{ \"token\":\"${TOKEN}\"}"`
-	echo $RET_REVOKE
+	RET_REVOKE=`curl -sS --fail -b /tmp/cookie.txt -X POST http://localhost:8091/MyDongleCloud/Auth/revoke -sS --failession -H "Content-Type: application/json" -d "{ \"token\":\"${TOKEN}\"}"`
+	echo $RET_REVOKE | jq
 fi
 if [ $SIGNOUT = 1 ]; then
 	echo "############### Sign-out"
-	RET_SIGNOUT=`curl -s -b /tmp/cookie.txt http://localhost:8091/MyDongleCloud/Auth/sign-out -H "Content-Type: application/json"`
-	echo $RET_SIGNOUT
+	RET_SIGNOUT=`curl -sS --fail -b /tmp/cookie.txt http://localhost:8091/MyDongleCloud/Auth/sign-out -H "Content-Type: application/json"`
+	echo $RET_SIGNOUT | jq
 fi
 if [ $DELETE = 1 ]; then
 	echo "############### Delete"
-	RET_DELETE=`curl -s -b /tmp/cookie.txt -X POST http://localhost:8091/MyDongleCloud/Auth/delete-user -H "Content-Type: application/json" -d '{"password":"demodemo"}' -c /tmp/cookie.txt`
-	echo $RET_DELETE
+	RET_DELETE=`curl -sS --fail -b /tmp/cookie.txt -X POST http://localhost:8091/MyDongleCloud/Auth/delete-user -H "Content-Type: application/json" -d '{"password":"demodemo"}' -c /tmp/cookie.txt`
+	echo $RET_DELETE | jq
 fi
