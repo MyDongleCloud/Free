27e04630007b8d0c75d7c71f635aeb52df6140e3
Gregoire Gentil <gregoire@gentil.com>
2025-10-17 01:29:36 -0700

    Configure MeTube module in dongle image

--- private/install.sh
+++ private/install.sh
@@ -691,6 +691,26 @@ cd /usr/local/modules/osticket
 ln -sf /disk/admin/.modules/osticket/ost-config.php include/ost-config.php
 
 echo "################################"
+echo "MeTube"
+echo "################################"
+cd /usr/local/modules/metube
+/home/mdc/rootfs/usr/local/modules/mydonglecloud/pip.sh -f /usr/local/modules/metube/env -v 3.12 -s
+echo "PATH before any modif: $PATH"
+PATHOLD=$PATH
+PATH=/usr/local/modules/metube/env/bin:$PATHOLD
+export PATH=/usr/local/modules/metube/env/bin:$PATHOLD
+echo "PATH new: $PATH python: `python --version`"
+pip install aiohttp yt_dlp mutagen curl-cffi watchfiles python-socketio
+PATH=$PATHOLD
+export PATH=$PATHOLD
+echo "PATH restored: $PATH"
+cd ui
+npm ci
+node_modules/.bin/ng build --configuration production
+mkdir /disk/admin/.modules/metube
+ln -sf /etc/systemd/system/metube.service /etc/systemd/system/multi-user.target.wants/metube.service
+
+echo "################################"
 echo "webtrees"
 echo "################################"
 cd /usr/local/modules/webtrees
