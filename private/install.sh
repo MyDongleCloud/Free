4277454438d1b554cfdb9218d260f27283c140e5
Gregoire Gentil <gregoire@gentil.com>
2025-10-19 02:03:50 -0700

    Configure Transmission module in dongle image

--- private/install.sh
+++ private/install.sh
@@ -495,6 +495,7 @@ echo "################################"
 echo "Transmission"
 echo "################################"
 apt-get -y install transmission-common transmission-daemon transmission-cli
+sed -i -e "s|^User=.*|User=admin\nEnvironment=TRANSMISSION_HOME=/disk/admin/.modules/transmission|" /usr/lib/systemd/system/transmission-daemon.service
 
 echo "################################"
 echo "uMTP"
