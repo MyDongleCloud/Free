d06314be2e5398126ae616a927f116bf35c9f0c9
Gregoire Gentil <gregoire@gentil.com>
2025-10-22 20:45:20 -0700

    Re-order alphabatically modules in setup script

--- rootfs/usr/local/modules/mydonglecloud/setup.sh
+++ rootfs/usr/local/modules/mydonglecloud/setup.sh
@@ -43,10 +43,10 @@ elif [ $1 = "roundcube" ]; then
 	su admin -c "/usr/local/modules/mydonglecloud/scripts/roundcube.sh -r"
 elif [ $1 = "tubesync" ]; then
 	su admin -c "/usr/local/modules/mydonglecloud/scripts/tubesync.sh -r"
-elif [ $1 = "yourls" ]; then
-	su admin -c "/usr/local/modules/mydonglecloud/scripts/yourls.sh -r"
 elif [ $1 = "webtrees" ]; then
 	/usr/local/modules/mydonglecloud/scripts/webtrees.sh -r
+elif [ $1 = "yourls" ]; then
+	su admin -c "/usr/local/modules/mydonglecloud/scripts/yourls.sh -r"
 else
 	echo "#Doing nothing for $1##################"
 fi
