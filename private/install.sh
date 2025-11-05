e21d3b5bec6587d7843c8c3c0324d102a2e870f2
Gregoire Gentil <gregoire@gentil.com>
2025-11-04 22:16:39 -0800

    Clean up install script for maximum coherency

--- private/install.sh
+++ private/install.sh
@@ -264,8 +264,8 @@ apt-get -y install openjdk-21-jdk openjdk-21-jre openjdk-21-jre-headless
 echo "################################"
 echo "jitsi"
 echo "################################"
-curl https://download.jitsi.org/jitsi-key.gpg.key | gpg --dearmor > /usr/share/keyrings/jitsi-keyring.gpg
-echo "deb [arch=arm64 signed-by=/usr/share/keyrings/jitsi-keyring.gpg] https://download.jitsi.org stable/" > /etc/apt/sources.list.d/jitsi-stable.list
+curl -fsSL https://download.jitsi.org/jitsi-key.gpg.key | gpg --dearmor -o /etc/apt/keyrings/jitsi.gpg
+echo "deb [arch=arm64 signed-by=/etc/apt/keyrings/jitsi.gpg] https://download.jitsi.org stable/" > /etc/apt/sources.list.d/jitsi.list
 apt-get update
 cat /home/mdc/private/preseed_jitsi.cfg | debconf-set-selections
 apt-get -y install jitsi-videobridge2 jitsi-meet-web-config jitsi-meet-web
