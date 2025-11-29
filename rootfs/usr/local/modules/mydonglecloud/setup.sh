40e82b9269512f40c2aa7147500dc3ec7f9c7a47
Gregoire Gentil <gregoire@gentil.com>
2025-11-28 16:46:57 -0800

    Avoid hard-coded paths in setup script of dongle image

--- rootfs/usr/local/modules/mydonglecloud/setup.sh
+++ rootfs/usr/local/modules/mydonglecloud/setup.sh
@@ -21,11 +21,12 @@ do
 	esac
 done
 
+PP=`dirname $0`
 module() {
 	if [ $2 = 1 ]; then
-		su admin -c "/usr/local/modules/mydonglecloud/scripts/$1.sh -r"
+		su admin -c "$PP/scripts/$1.sh -r"
 	else
-		/usr/local/modules/mydonglecloud/scripts/$1.sh -r
+		$PP/scripts/$1.sh -r
 	fi
 	COUNT=$((COUNT + 1))
 	P=$(($COUNT * 100 / $TOTAL))
@@ -33,7 +34,7 @@ module() {
 }
 
 if [ $ALL = 1 ]; then
-	TOTAL=`jq '[.[] | select(has("setup"))] | length' /usr/local/modules/mydonglecloud/modulesdefault.json`
+	TOTAL=`jq '[.[] | select(has("setup"))] | length' $PP/modulesdefault.json`
 	COUNT=0
 	module meilisearch 1
 	module mongodb 0
@@ -58,43 +59,43 @@ if [ $ALL = 1 ]; then
 fi
 
 if [ $1 = "apache2" ]; then
-	su admin -c "/usr/local/modules/mydonglecloud/scripts/apache2.sh -r"
+	su admin -c "$PP/scripts/apache2.sh -r"
 elif [ $1 = "bugzilla" ]; then
-	/usr/local/modules/mydonglecloud/scripts/bugzilla.sh -r
+	$PP/scripts/bugzilla.sh -r
 elif [ $1 = "homeassistant" ]; then
-	su admin -c "/usr/local/modules/mydonglecloud/scripts/homeassistant.sh -r"
+	su admin -c "$PP/scripts/homeassistant.sh -r"
 elif [ $1 = "jitsimeet" ]; then
-	/usr/local/modules/mydonglecloud/scripts/jitsimeet.sh -r
+	$PP/scripts/jitsimeet.sh -r
 elif [ $1 = "librechat" ]; then
-	su admin -c "/usr/local/modules/mydonglecloud/scripts/librechat.sh -r"
+	su admin -c "$PP/scripts/librechat.sh -r"
 elif [ $1 = "mantisbugtracker" ]; then
-	su admin -c "/usr/local/modules/mydonglecloud/scripts/mantisbugtracker.sh -r"
+	su admin -c "$PP/scripts/mantisbugtracker.sh -r"
 elif [ $1 = "meilisearch" ]; then
-	su admin -c "/usr/local/modules/mydonglecloud/scripts/meilisearch.sh -r"
+	su admin -c "$PP/scripts/meilisearch.sh -r"
 elif [ $1 = "metube" ]; then
-	su admin -c "/usr/local/modules/mydonglecloud/scripts/metube.sh -r"
+	su admin -c "$PP/scripts/metube.sh -r"
 elif [ $1 = "mongodb" ]; then
-	/usr/local/modules/mydonglecloud/scripts/mongodb.sh -r
+	$PP/scripts/mongodb.sh -r
 elif [ $1 = "mysql" ]; then
-	/usr/local/modules/mydonglecloud/scripts/mysql.sh -r
+	$PP/scripts/mysql.sh -r
 elif [ $1 = "osticket" ]; then
-	su admin -c "/usr/local/modules/mydonglecloud/scripts/osticket.sh -r"
+	su admin -c "$PP/scripts/osticket.sh -r"
 elif [ $1 = "postfix" ]; then
-	/usr/local/modules/mydonglecloud/scripts/postfix.sh -r
+	$PP/scripts/postfix.sh -r
 elif [ $1 = "postgresql" ]; then
-	/usr/local/modules/mydonglecloud/scripts/postgresql.sh -r
+	$PP/scripts/postgresql.sh -r
 elif [ $1 = "privatebin" ]; then
-	/usr/local/modules/mydonglecloud/scripts/privatebin.sh -r
+	$PP/scripts/privatebin.sh -r
 elif [ $1 = "projectsend" ]; then
-	/usr/local/modules/mydonglecloud/scripts/projectsend.sh -r
+	$PP/scripts/projectsend.sh -r
 elif [ $1 = "roundcube" ]; then
-	su admin -c "/usr/local/modules/mydonglecloud/scripts/roundcube.sh -r"
+	su admin -c "$PP/scripts/roundcube.sh -r"
 elif [ $1 = "tubesync" ]; then
-	su admin -c "/usr/local/modules/mydonglecloud/scripts/tubesync.sh -r"
+	su admin -c "$PP/scripts/tubesync.sh -r"
 elif [ $1 = "webtrees" ]; then
-	/usr/local/modules/mydonglecloud/scripts/webtrees.sh -r
+	$PP/scripts/webtrees.sh -r
 elif [ $1 = "yourls" ]; then
-	su admin -c "/usr/local/modules/mydonglecloud/scripts/yourls.sh -r"
+	su admin -c "$PP/scripts/yourls.sh -r"
 else
 	echo "#Doing nothing for $1##################"
 fi
