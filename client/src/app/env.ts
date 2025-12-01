23394b655c30ef2270de3e1020ee903012e6144c
Gregoire Gentil <gregoire@gentil.com>
2025-12-01 14:15:00 -0800

    Fix hack to change color of png icon in client app

--- client/src/app/env.ts
+++ client/src/app/env.ts
@@ -391,17 +391,17 @@ colorWord(st, bckgd = true) {
 
 colorWord2(st) {
 	if (st == "Essential")
-		return "translate-y-[-96px] drop-shadow-[0px_96px_0_var(--color--yellow-600)]";
+		return "translate-y-[-256px] drop-shadow-[0px_256px_0_var(--color--yellow-600)]";
 	else if (st == "Personal")
-		return "translate-y-[-96px] drop-shadow-[0px_96px_0_var(--color--blue-600)]";
+		return "translate-y-[-256px] drop-shadow-[0px_256px_0_var(--color--blue-600)]";
 	else if (st == "Productivity")
-		return "translate-y-[-96px] drop-shadow-[0px_96px_0_var(--color--purple-600)]";
+		return "translate-y-[-256px] drop-shadow-[0px_256px_0_var(--color--purple-600)]";
 	else if (st == "Utils")
-		return "translate-y-[-96px] drop-shadow-[0px_96px_0_var(--color--cyan-600)]";
+		return "translate-y-[-256px] drop-shadow-[0px_256px_0_var(--color--cyan-600)]";
 	else if (st == "Developer")
-		return "translate-y-[-96px] drop-shadow-[0px_96px_0_var(--color--red-600)]";
+		return "translate-y-[-256px] drop-shadow-[0px_256px_0_var(--color--red-600)]";
 	else
-		return "translate-y-[-96px] drop-shadow-[0px_96px_0_var(--color--gray-600)]";
+		return "translate-y-[-256px] drop-shadow-[0px_256px_0_var(--color--gray-600)]";
 }
 
 colorPercent(p, limits = [ 80, 50 ]) {
