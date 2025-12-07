2910acf381f4ba7923f1ca1b072b4b253252c02f
Gregoire Gentil <gregoire@gentil.com>
2025-12-07 14:54:06 -0800

    Remove config message in issue for Cockpit module

--- private/install.sh
+++ private/install.sh
@@ -217,6 +217,7 @@ echo "################################"
 echo "cockpit"
 echo "################################"
 apt-get -y install cockpit
+rm /etc/issue.d/cockpit.issue
 
 echo "################################"
 echo "postgresql"
