70a315d28e9858a05dc041aa081052f898776999
Gregoire Gentil <gregoire@gentil.com>
2025-12-12 14:02:04 -0800

    Scramble autologin inserted javascript script in custom Apache2 module

--- moduleApache2/prepare.sh
+++ moduleApache2/prepare.sh
@@ -19,6 +19,11 @@ done
 if [ $MINIFY = 1 ]; then
 	JS=$(cat login.js | sed 's/\&/\\&/g')
 	JS=$(echo $JS | tr '\n' ' ')
+	SEARCH=$(grep -oE '\<_app_[a-zA-Z0-9_]*\>' login.js | sort -u | tr '\n' ' ')
+	for ss in $SEARCH; do
+		rr=$(tr -dc 'A-Za-z' < /dev/urandom | head -c 16)
+		JS=$(echo $JS | sed "s|$ss|$rr|g")
+	done
 	JS=$(echo $JS | sed 's/"/\\\\"/g')
 	sed "/#define INJECTION/d" login.c | sed "s|</script>\"|#define INJECTION \"<script type='module'>${JS}</script>\"|" > login_.c
 else
