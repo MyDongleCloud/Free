f8156ce39f6ae040eb62245a9546e9b247e84fa4
Gregoire Gentil <gregoire@gentil.com>
2025-12-11 20:41:11 -0800

    Don't symlink app.module (from client app) in login app

--- login/src/app/app.module.ts
+++ login/src/app/app.module.ts
@@ -1,1 +1,54 @@
-../../../client/src/app/app.module.ts
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { provideHttpClient, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { forkJoin, Observable, map } from 'rxjs';
+
+export class MultiHttpLoader implements TranslateLoader {
+constructor(private http: HttpClient, private configs: { prefix: string, suffix: string }[]) {}
+
+getTranslation(lang: string): Observable<any> {
+	const requests = this.configs.map(config => {
+		const path = `${config.prefix}${lang}${config.suffix}`;
+		return this.http.get(path);
+	});
+	return forkJoin(requests).pipe(map(responses => {
+		return responses.reduce((acc, current) => ({ ...acc, ...current }), {});
+	}));
+}
+
+}
+
+export function createMultiHttpLoader(http: HttpClient) {
+	return new MultiHttpLoader(http, [
+		{ prefix: 'assets/i18n/global-', suffix: '.json' },
+		{ prefix: 'assets/i18n/pages-', suffix: '.json' }
+	]);
+}
+
+@NgModule({
+	declarations: [AppComponent],
+	imports: [
+		BrowserModule,
+		IonicModule.forRoot(),
+		AppRoutingModule,
+		TranslateModule.forRoot({
+			loader: {
+				provide: TranslateLoader,
+				useFactory: createMultiHttpLoader,
+				deps: [HttpClient]
+			},
+			fallbackLang: "en"
+		})],
+	providers: [
+		{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+		provideHttpClient()
+	],
+	bootstrap: [AppComponent],
+})
+
+export class AppModule {}
