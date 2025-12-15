ed8dda463deb06f2f7f7786859dcf92c4807d3bd
Gregoire Gentil <gregoire@gentil.com>
2025-12-15 14:37:19 -0800

    Fix install of module Tabby by copying compiled executables

--- private/modules/install/tabby.sh
+++ private/modules/install/tabby.sh
@@ -4,3 +4,5 @@ apt-get -y install protobuf-compiler libopenblas-dev graphviz
 cd /usr/local/modules/tabby
 echo 3 > /proc/sys/vm/drop_caches
 cargo build --release
+cp /usr/local/modules/tabby/target/release/llama-server /usr/local/bin
+cp /usr/local/modules/tabby/target/release/tabby /usr/local/bin
