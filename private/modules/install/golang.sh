5f9d0e7aa22f11417a3a52e38a6c8f7e51069d89
Gregoire Gentil <gregoire@gentil.com>
2025-12-17 12:52:25 -0800

    Upgrade and change installation method of module Go

---
+++ private/modules/install/golang.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd /home/ai/build
+wget -nv https://go.dev/dl/go1.25.5.linux-arm64.tar.gz
+rm -rf /usr/local/go
+tar -C /usr/local -xzf go1.25.5.linux-arm64.tar.gz
+ln -sf /usr/local/go/bin/go /usr/local/bin
+ln -sf /usr/local/go/bin/gofmt /usr/local/bin
