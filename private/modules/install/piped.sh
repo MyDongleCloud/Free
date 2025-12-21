5b1ec852bcc478fcd7022910781ad90449d94fe4
Gregoire Gentil <gregoire@gentil.com>
2025-12-20 20:42:21 -0800

    Improve Piped module

--- private/modules/install/piped.sh
+++ private/modules/install/piped.sh
@@ -2,6 +2,8 @@
 
 cd /usr/local/modules/piped
 npm install
+sync
+echo 3 > /proc/sys/vm/drop_caches
 npm run build
 find dist -type f -exec sed -i -e 's@https://pipedapi.kavin.rocks@" + window.location.origin + "/_api_@g'  {} \;
 find dist -type f -exec sed -i -e 's@https://pipedproxy.kavin.rocks@" + window.location.origin + "/_proxy_@g' {} \;
@@ -18,4 +20,4 @@ cargo build --release
 sync
 echo 3 > /proc/sys/vm/drop_caches
 cp /usr/local/modules/piped/pipedproxy/target/release/piped-proxy /usr/local/bin
-#rm -rf /usr/local/modules/tabby/target/
+rm -rf /usr/local/modules/tabby/target/
