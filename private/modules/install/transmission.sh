54a9fcf001f5840d13c6a4ccd750b30623299e55
Gregoire Gentil <gregoire@gentil.com>
2025-12-20 20:35:49 -0800

    Improve Transmission module (cont'd)

--- private/modules/install/transmission.sh
+++ private/modules/install/transmission.sh
@@ -2,3 +2,4 @@
 
 apt-get -y install transmission-common transmission-daemon transmission-cli
 sed -i -e "s|^User=.*|User=admin\nEnvironment=TRANSMISSION_HOME=/disk/admin/modules/transmission|" /usr/lib/systemd/system/transmission-daemon.service
+systemctl disable transmission-daemon.service
