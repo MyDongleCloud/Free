f30249f377e39cd74ca201fe1c6da172bed64bfe
Gregoire Gentil <gregoire@gentil.com>
2025-12-22 22:10:41 -0800

    Implement Syncthing module

---
+++ private/modules/reset/syncthing.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+helper() {
+echo "*******************************************************"
+echo "Usage for syncthing [-h -r]"
+echo "h:	Print this usage and exit"
+echo "r:	Reset"
+exit 0
+}
+
+if [ "m`id -u`" = "m0" ]; then
+	echo "You should not be root"
+	exit 0
+fi
+
+RESET=0
+while getopts hr opt
+do
+	case "$opt" in
+		h) helper;;
+		r) RESET=1;;
+	esac
+done
+
+if [ $RESET != 1 ]; then
+	exit 0
+fi
+
+echo "#Reset syncthing##################"
+systemctl stop syncthing.service
+rm -rf /disk/admin/modules/syncthing
+mkdir /disk/admin/modules/syncthing
+systemctl start syncthing.service
+systemctl enable syncthing.service
