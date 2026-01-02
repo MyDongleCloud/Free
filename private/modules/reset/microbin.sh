92cc73a456ae39ad8d21c108070ea8adb2402215
Gregoire Gentil <gregoire@gentil.com>
2026-01-01 22:25:06 -0800

    Fix typo in reset script of module MicroBin

--- private/modules/reset/microbin.sh
+++ private/modules/reset/microbin.sh
@@ -31,8 +31,8 @@ systemctl stop microbin.service
 rm -rf /disk/admin/modules/microbin
 mkdir /disk/admin/modules/microbin
 cp /usr/local/modules/microbin/.env /disk/admin/modules/microbin/
-sed -i -e 's/export MICROBIN_PORT=8080/export MICROBIN_PORT=8106/' /disk/admin/modules/microbin/.env
-sed -i -e 's/export MICROBIN_BIND="0.0.0.0"/export MICROBIN_BIND="127.0.0.1"/' /disk/admin/modules/microbin/.env
-sed -i -e 's/export MICROBIN_DATA_DIR="microbin_data"/export MICROBIN_DATA_DIR="/disk/admin/modules/microbin"/' /disk/admin/modules/microbin/.env
+sed -i -e 's@export MICROBIN_PORT=8080@export MICROBIN_PORT=8106@' /disk/admin/modules/microbin/.env
+sed -i -e 's@export MICROBIN_BIND="0.0.0.0"@export MICROBIN_BIND="127.0.0.1"@' /disk/admin/modules/microbin/.env
+sed -i -e 's@export MICROBIN_DATA_DIR="microbin_data"@export MICROBIN_DATA_DIR="/disk/admin/modules/microbin"@' /disk/admin/modules/microbin/.env
 systemctl start microbin.service
 systemctl enable microbin.service
