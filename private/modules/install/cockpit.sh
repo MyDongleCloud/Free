3cc66465ef5209a14f9237390fdf10354c4a9aa7
Gregoire Gentil <gregoire@gentil.com>
2026-01-01 21:30:15 -0800

    Fix reverse proxy of Cockpit module in image

--- private/modules/install/cockpit.sh
+++ private/modules/install/cockpit.sh
@@ -2,3 +2,8 @@
 
 apt-get -y install cockpit
 rm -f /etc/issue.d/cockpit.issue /run/cockpit/active.issue /usr/share/cockpit/issue/update-issue
+cat > /etc/cockpit/cockpit.conf << EOF
+[WebService]
+ProtocolHeader = X-Forwarded-Proto
+AllowUnencrypted = true
+EOF
