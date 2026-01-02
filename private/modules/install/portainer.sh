536121351ade2637bc2cba0a64f2b9ae2cc28868
Gregoire Gentil <gregoire@gentil.com>
2026-01-01 21:26:17 -0800

    Add Portainer module to image

---
+++ private/modules/install/portainer.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cd /home/ai/build
+wget -nv https://github.com/portainer/portainer/releases/download/2.33.6/portainer-2.33.6-linux-arm64.tar.gz
+tar -xpf portainer*
+mv portainer /usr/local/modules/
---
+++ private/modules/reset/portainer.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+helper() {
+echo "*******************************************************"
+echo "Usage for portainer [-h -r]"
+echo "h:	Print this usage and exit"
+echo "r:	Reset"
+exit 0
+}
+
+if [ "m`id -u`" = "m0" ]; then
+	echo "You should not be root"
+	exit 0
+fi
+
+RESET=0
+while getopts hr opt
+do
+	case "$opt" in
+		h) helper;;
+		r) RESET=1;;
+	esac
+done
+
+if [ $RESET != 1 ]; then
+	exit 0
+fi
+
+echo "#Reset portainer##################"
+systemctl stop portainer.service
+rm -rf /disk/admin/modules/portainer
+mkdir /disk/admin/modules/portainer
+systemctl start portainer.service
+systemctl enable portainer.service
