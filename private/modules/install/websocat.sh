5057db421add0e73b7dfc6334faf7ee8bfd16db6
Gregoire Gentil <gregoire@gentil.com>
2026-01-17 21:13:50 -0800

    Add websocat and wscat modules

---
+++ private/modules/install/websocat.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cd /home/ai/build
+wget -nv https://github.com/vi/websocat/releases/download/v1.14.1/websocat.aarch64-unknown-linux-musl
+chmod a+x websocat.aarch64-unknown-linux-musl
+mv websocat.aarch64-unknown-linux-musl /usr/local/bin/websocat
