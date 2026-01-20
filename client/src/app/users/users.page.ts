58b780fff2dcf9d49e8c3e589c8fef90eca6d0bd
Gregoire Gentil <gregoire@gentil.com>
2026-01-20 00:36:32 -0800

    Let admin edit users info in users page of client app

--- client/src/app/users/users.page.ts
+++ client/src/app/users/users.page.ts
@@ -24,8 +24,59 @@ constructor(public global: Global, private cdr: ChangeDetectorRef, private httpC
 
 async getData() {
 	this.users = await this.httpClient.get("/_app_/auth/admin/list-users", {headers:{"content-type": "application/json"}}).toPromise();
-	this.global.consolelog(2, "Auth list-users: ", this.users);
+	this.global.consolelog(2, "Auth admin/list-users: ", this.users);
 }
 
+async setEmail(userId, event) {
+	const email = (event.target as HTMLSelectElement).value;
+	const data = { userId, data:{ email } };
+	let ret = null;
+	try {
+		ret = await this.httpClient.post("/_app_/auth/admin/update-user", JSON.stringify(data), {headers:{"content-type": "application/json"}}).toPromise();
+		this.global.consolelog(2, "Auth admin/update-user: ", ret);
+	} catch(e) { alert(e.error.message); }
+}
+
+async setUsername(userId, event) {
+	const username = (event.target as HTMLSelectElement).value;
+	const data = { userId, data:{ username } };
+	let ret = null;
+	try {
+		ret = await this.httpClient.post("/_app_/auth/admin/update-user", JSON.stringify(data), {headers:{"content-type": "application/json"}}).toPromise();
+		this.global.consolelog(2, "Auth admin/update-user: ", ret);
+	} catch(e) { alert(e.error.message); }
+}
+
+async setRole(userId, event) {
+	const role = (event.target as HTMLSelectElement).value;
+	const data = { userId, role };
+	let ret = null;
+	try {
+		ret = await this.httpClient.post("/_app_/auth/admin/set-role", JSON.stringify(data), {headers:{"content-type": "application/json"}}).toPromise();
+		this.global.consolelog(2, "Auth admin/set-role: ", ret);
+	} catch(e) { alert(e.error.message); }
+}
+
+async resetPassword(userId) {
+	const newPassword = Math.random().toString(36).substring(2, 12);
+	const data = { userId, newPassword };
+	let ret = null;
+	try {
+		ret = await this.httpClient.post("/_app_/auth/admin/set-user-password", JSON.stringify(data), {headers:{"content-type": "application/json"}}).toPromise();
+		this.global.consolelog(2, "Auth admin/set-user-password: ", ret);
+	} catch(e) { alert(e.error.message); return; }
+	this.global.presentToast("The new password is:<br>" + newPassword, "alert-outline", 0);
+}
+
+async setApproved(userId, event) {
+	const approved = (event.target as HTMLInputElement).checked;
+alert(approved);
+	const data = { userId, data:{ approved } };
+	let ret = null;
+	try {
+		ret = await this.httpClient.post("/_app_/auth/admin/update-user", JSON.stringify(data), {headers:{"content-type": "application/json"}}).toPromise();
+		this.global.consolelog(2, "Auth admin/update-user: ", ret);
+	} catch(e) { alert(e.error.message); }
+}
 
 }
