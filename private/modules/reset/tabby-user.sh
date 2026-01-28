8a6dcc4587512d317839d5ab2fb55851aa2aac6d
Gregoire Gentil <gregoire@gentil.com>
2026-01-27 19:35:50 -0800

    Fix user reset script to register the external URL for Tabby module

--- private/modules/reset/tabby-user.sh
+++ private/modules/reset/tabby-user.sh
@@ -49,11 +49,16 @@ PASSWD=$(pwgen -B -c -y -n -r "\"\!\'\`\$@~#%^&*()+={[}]|:;<>?/" 12 1)
 EMAIL="admin@$CLOUDNAME.mydongle.cloud"
 URL2="https://aicode.$CLOUDNAME.mydongle.cloud"
 
-curl -sS --fail -o /tmp/tabby.txt -X POST $URL/graphql -H "Content-Type: application/json" --data-binary "{\"operationName\":\"register\",\"query\":\"mutation register(\$name: String!, \$email: String!, \$password1: String!, \$password2: String!, \$invitationCode: String) {\n register(\n name: \$name\n email: \$email\n password1: \$password1\n password2: \$password2\n invitationCode: \$invitationCode\n ) {\n accessToken\n refreshToken\n __typename\n }\n}\",\"variables\":{\"email\":\"$EMAIL\",\"name\":\"$CLOUDNAME\",\"password1\":\"$PASSWD\",\"password2\":\"$PASSWD\"}}"
+curl -sS --fail -o /tmp/tabby.txt -X POST $URL/graphql -H "Content-Type: application/json" --data-binary "{ \"operationName\":\"register\", \"query\":\"mutation register(\$name: String!, \$email: String!, \$password1: String!, \$password2: String!, \$invitationCode: String) { register( name: \$name email: \$email password1: \$password1 password2: \$password2 invitationCode: \$invitationCode ) { accessToken refreshToken __typename } }\", \"variables\":{\"email\":\"$EMAIL\", \"name\":\"$CLOUDNAME\", \"password1\":\"$PASSWD\", \"password2\":\"$PASSWD\" } }"
 token=`jq -r ".data.register.accessToken" /tmp/tabby.txt`
+#echo $token
 rm -f /tmp/tabby.txt
-echo "{\"name\":\"${CLOUDNAME}\", \"email\":\"${EMAIL}\", \"password\":\"${PASSWD}\"}" > /disk/admin/modules/_config_/tabby.json
-respone=`curl -sS -X POST $URL/graphql -H "Content-Type: application/json" -H "Authorization: Bearer $token" --data-binary "{\"operationName\":\"updateNetworkSettingMutation\",\"query\":\"mutation updateNetworkSettingMutation(\$input: NetworkSettingInput!) {\n updateNetworkSetting(input: \$input)\n}\",\"variables\":{\"input\":{\"externalUrl\":\"$URL2\"}}}"`
+
+data="{ \"operationName\":\"updateNetworkSettingMutation\", \"query\":\"mutation updateNetworkSettingMutation(\$input: NetworkSettingInput) { updateNetworkSetting(input: \$input) }\", \"variables\":{ \"input\":{ \"externalUrl\":\"https://aicode.gregoiregentil.mydongle.cloud\" } } }"
+data=`echo $data | sed -e 's/NetworkSettingInput/NetworkSettingInput!/'`
+response=`curl -sS -X POST http://localhost:8100/graphql -H "Content-Type: application/json" -H "Authorization: Bearer $token" --data-binary "$data"`
 #echo $response
+
+echo "{\"name\":\"${CLOUDNAME}\", \"email\":\"${EMAIL}\", \"password\":\"${PASSWD}\"}" > /disk/admin/modules/_config_/tabby.json
 
 echo {" \"a\":\"status\", \"module\":\"$(basename $0 -user.sh)\", \"state\":\"finish\" }" | websocat -1 ws://localhost:8094
