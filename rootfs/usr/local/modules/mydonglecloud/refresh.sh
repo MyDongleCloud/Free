357d6246534834281e9e11e2eebf7038de04d7b8
Gregoire Gentil <gregoire@gentil.com>
2026-01-28 00:12:45 -0800

    Add helper to refresh script

--- rootfs/usr/local/modules/mydonglecloud/refresh.sh
+++ rootfs/usr/local/modules/mydonglecloud/refresh.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
+helper() {
+echo "*******************************************************"
+echo "Usage for refresh [-h]"
+echo "h:	Print this usage and exit"
+exit 0
+}
+
+while getopts h opt
+do
+	case "$opt" in
+		h) helper;;
+	esac
+done
+
 echo "{\"a\":\"refresh-webserver\"}" | websocat -1 ws://localhost:8094
