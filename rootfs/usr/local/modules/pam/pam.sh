856fe169baa854a97c164d35659be3ffbabf79b6
Gregoire Gentil <gregoire@gentil.com>
2026-01-28 14:17:24 -0800

    Update and fix pam script in image

--- rootfs/usr/local/modules/pam/pam.sh
+++ rootfs/usr/local/modules/pam/pam.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
-json_payload=$(printf '%s\n' "$PAM_USER" "$PAM_SERVICE" "$PAM_TYPE" "$1" | jq -Rn '{ a: "pam", u:input, s:input, t:input, o:input }')
-echo $json_payload | nc -w 1 127.0.0.1 8093
+helper() {
+echo "*******************************************************"
+echo "Usage for pam [-h]"
+echo "h:	Print this usage and exit"
+exit 0
+}
+
+while getopts h opt
+do
+	case "$opt" in
+		h) helper;;
+	esac
+done
+
+json_payload=$(printf '%s\n' "$PAM_USER" "$PAM_SERVICE" "$PAM_TYPE" "$1" | jq -Rn '{ a:"pam", u:input, s:input, t:input, o:input }')
+echo $json_payload | websocat -1 ws://localhost:8094
 exit 0
