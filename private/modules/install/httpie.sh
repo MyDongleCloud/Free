6f5a1721c8bb66ec40dd129897940a0757e8e235
Gregoire Gentil <gregoire@gentil.com>
2026-02-09 10:22:35 -0800

    Add module HTTPie

---
+++ private/modules/install/httpie.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+apt-get -y install python3-defusedxml python3-multidict python3-pip python3-requests-toolbelt python3-socks
+cd /home/ai/build
+wget -nv http://deb.debian.org/debian/pool/main/h/httpie/httpie_3.2.4-3_all.deb
+dpkg -i httpie*.deb
