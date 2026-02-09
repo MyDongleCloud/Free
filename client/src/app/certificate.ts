75d77fec0cf628dd3b2e8a3538a85417fd77e0ee
Gregoire Gentil <gregoire@gentil.com>
2026-02-09 10:28:22 -0800

    Use global log for certificates in client and login apps

--- client/src/app/certificate.ts
+++ client/src/app/certificate.ts
@@ -13,29 +13,23 @@ import * as ENC from '@root/encoding/base64';
 })
 
 export class Certificate {
-developer: boolean = false;
 SERVERURL: string = "https://mydongle.cloud";
 
 constructor(private global: Global, private httpClient: HttpClient) {}
-
-consolelog(level, ...st) {
-	if (level == 0 || this.developer)
-		console.log(...st);
-}
 
 async process(name, shortname, additionalDomains) {
 	const ret = { accountKey:"", accountKeyId:"", fullChain:"", privateKey:"" };
 	const DOMAINNAME = ["mydongle.cloud", "mondongle.cloud", "myd.cd"];//.concat(additionalDomains);
 	const STAGING = true;
-	let acme = ACME.create({ maintainerEmail:"acme@@mydongle.cloud", packageAgent:"MDC/2025-01-01", notify:function (ev, msg) { /*this.consolelog(1, msg);*/ }, skipDryRun:true/*, skipChallengeTest:true*/ });
+	let acme = ACME.create({ maintainerEmail:"acme@@mydongle.cloud", packageAgent:"MDC/2025-01-01", notify:function (ev, msg) { /*this.global.consolelog(1, msg);*/ }, skipDryRun:true/*, skipChallengeTest:true*/ });
 	await acme.init("https://acme" + (STAGING ? "-staging" : "") + "-v02.api.letsencrypt.org/directory");
 
 	const accountKeypair = await Keypairs.generate({ kty: "EC", format: "jwk" });
 	const accountKey = accountKeypair.private;
 	ret.accountKey = await Keypairs.export({ jwk: accountKey });
-	this.consolelog(2, "ACME: Registering account...");
+	this.global.consolelog(2, "ACME: Registering account...");
 	const account = await acme.accounts.create({ subscriberEmail:"certificate@mydongle.cloud", agreeToTerms:true, accountKey:accountKey });
-	this.consolelog(2, "ACME: Created account with id ", account.key.kid);
+	this.global.consolelog(2, "ACME: Created account with id ", account.key.kid);
 	ret.accountKeyId = account.key.kid;
 
 	const serverKeypair = await Keypairs.generate({ kty: "RSA", format: "jwk" });
@@ -50,7 +44,7 @@ async process(name, shortname, additionalDomains) {
 		return acc.concat(subDomains);
 	}, [] as string[]);
 	const domainsNb = domains.length;
-	this.consolelog(2, "ACME: Domains:" + domains.join(" "));
+	this.global.consolelog(2, "ACME: Domains:" + domains.join(" "));
 	const csrDer = await CSR.csr({ jwk:serverKey, domains, encoding:"der" });
 	const csr = PEM.packBlock({ type:"CERTIFICATE REQUEST", bytes:csrDer });
 	const lines = [];
@@ -61,35 +55,35 @@ async process(name, shortname, additionalDomains) {
 				return null;
 			},
 			zones: async ({ challenge }) => {
-				this.consolelog(2, "ACME: Zones", DOMAINNAME);
+				this.global.consolelog(2, "ACME: Zones", DOMAINNAME);
 				return DOMAINNAME;
 			},
 			set: async ({ challenge }) => {
 				const domain = (challenge?.wildcard ? "*." : "") + challenge.identifier.value;
-				this.consolelog(2, "ACME: Set " + domain);
+				this.global.consolelog(2, "ACME: Set " + domain);
 				//for (let i = 0; i < challenge.challenges.length; i++)
 					//if (challenge.challenges[i]["type"] == "dns-01") {
-						//this.consolelog(2, "ACME: Set" + (challenge?.wildcard ? " (.*)" : ""), challenge);
+						//this.global.consolelog(2, "ACME: Set" + (challenge?.wildcard ? " (.*)" : ""), challenge);
 					//}
 				const line = "_acme-challenge." + challenge.identifier.value + ". 1 IN TXT " + challenge.keyAuthorizationDigest + "\n";
-				this.consolelog(2, line);
+				this.global.consolelog(2, line);
 				lines.push(line);
 				if (domains.length == lines.length) {
 					const dataA = { lines, action:"add" };
 					const retA = await this.httpClient.post(this.SERVERURL + "/master/certificates.json", dataA).toPromise();
-					this.consolelog(2, retA);
+					this.global.consolelog(2, retA);
 					//alert("AuthorizationDigests sent to DNS server");
 				}
 			},
 			get: async (args) => {
-				//this.consolelog(2, "ACME: Get: ", args);
+				//this.global.consolelog(2, "ACME: Get: ", args);
 			},
 			remove: async ({ challenge }) => {
 				const domain = (challenge?.wildcard ? "*." : "") + challenge.identifier.value;
-				this.consolelog(2, "ACME: Remove " + domain);
+				this.global.consolelog(2, "ACME: Remove " + domain);
 				//for (let i = 0; i < challenge.challenges.length; i++)
 					//if (challenge.challenges[i]["type"] == "dns-01") {
-						//this.consolelog(2, "ACME: Remove" + (challenge?.wildcard ? " (.*)" : ""), challenge);
+						//this.global.consolelog(2, "ACME: Remove" + (challenge?.wildcard ? " (.*)" : ""), challenge);
 					//}
 			},
 			propagationDelay: 2000
@@ -98,14 +92,14 @@ async process(name, shortname, additionalDomains) {
 	try {
 		const pems = await acme.certificates.create({ account, accountKey, csr, domains, challenges });
 		ret.fullChain = pems.cert + "\n" + pems.chain + "\n";
-		this.consolelog(2, "##################################");
-		this.consolelog(2, "ACME: Expiration " + pems.expires);
-		this.consolelog(2, ret);
-		this.consolelog(2, "##################################");
+		this.global.consolelog(2, "##################################");
+		this.global.consolelog(2, "ACME: Expiration " + pems.expires);
+		this.global.consolelog(2, ret);
+		this.global.consolelog(2, "##################################");
 	} catch(e) {}
 	const dataD = { lines, action:"delete" };
 	const retD = await this.httpClient.post(this.SERVERURL + "/master/certificates.json", dataD).toPromise();
-	this.consolelog(2, retD);
+	this.global.consolelog(2, retD);
 	return ret;
 }
 
