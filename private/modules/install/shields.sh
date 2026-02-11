5855812e38e77d8145cefedc2b079362f4c3a1bb
Gregoire Gentil <gregoire@gentil.com>
2026-02-10 23:53:05 -0800

    Fix install script for module Shields

--- private/modules/install/shields.sh
+++ private/modules/install/shields.sh
@@ -4,4 +4,4 @@ cd /usr/local/modules/shields
 npm ci
 sync
 echo 3 > /proc/sys/vm/drop_caches
-NODE_OPTIONS="--max-old-space-size=4096" npm run build
+NODE_OPTIONS="--max-old-space-size=2048" npm run build
